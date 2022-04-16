3dfa35c29d661733fcd8e9f33f39fc7c57b61d51
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-17 01:53:04 +0200

    Create mutex.py

---
+++ src/python/asynchrony/mutex.py
@@ -0,0 +1,21 @@
+import asyncio
+import time
+
+async def foo(lock, id):
+    async with lock:
+        for _ in range(5):
+          print(f"Worker {id} is going to sleep")
+          await asyncio.sleep(1)
+    print(f"End of worker {id} function")
+
+async def task_generator():
+    lock = asyncio.Lock()
+    n_workers = 3
+    await asyncio.wait([foo(lock, i) for i in range(n_workers)])
+
+lock = asyncio.Lock()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(task_generator())
+loop.close()
+
+print("The End")
