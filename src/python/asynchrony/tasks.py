68da18349187deec0e6d7cece301dea829461b0b
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-17 01:46:22 +0200

    Create tasks.py

---
+++ src/python/asynchrony/tasks.py
@@ -0,0 +1,19 @@
+import asyncio
+import time
+
+async def example_task():
+    print("example task")
+    await asyncio.sleep(1)
+
+async def task_generator():
+    for i in range(5):
+        asyncio.ensure_future(example_task())
+    pending = asyncio.all_tasks()
+    for p in pending:
+      print(p)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(task_generator())
+loop.close()
+
+print('The End')
