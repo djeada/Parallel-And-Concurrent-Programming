b2cc335642c621db26621c86fc208912806b3b91
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-21 00:15:16 +0200

    Update single_thread.py

--- src/python/multithreading/single_thread.py
+++ src/python/multithreading/single_thread.py
@@ -7,14 +7,14 @@ def foo():
     print("Worker function")
     print(f"Worker function process id: {os.getpid()}")
     print(f"Worker thread id: {threading.currentThread().ident}")
-    print(f"Worker thread id: {threading.currentThread().getName()}")
+    print(f"Worker thread name: {threading.currentThread().getName()}")
 
 
 if __name__ == "__main__":
 
     print(f"Main function process id: {os.getpid()}")
     print(f"Main thread id: {threading.currentThread().ident}")
-    print(f"Main thread id: {threading.currentThread().getName()}")
+    print(f"Main thread name: {threading.currentThread().getName()}")
 
     thread = Thread(name="Example_thread", target=foo)
     thread.start()
