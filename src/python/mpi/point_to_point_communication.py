d9d02d8f39c3aecc3a3b0dad88ab165f562b5cb0
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-21 00:38:44 +0200

    Update point_to_point_communication.py

--- src/python/mpi/point_to_point_communication.py
+++ src/python/mpi/point_to_point_communication.py
@@ -2,26 +2,26 @@ from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.rank
-print("my rank is : ", rank)
+print(f"Hello from {rank}")
 
 if rank == 0:
     data = 10000000
     destination_process = 4
     comm.send(data, dest=destination_process)
-    print("sending data %s " % data + "to process %d" % destination_process)
+    print(f"{rank} sent {data} to {destination_process}")
 
 if rank == 1:
     destination_process = 8
     data = "hello"
     comm.send(data, dest=destination_process)
-    print("sending data %s :" % data + "to process %d" % destination_process)
+    print(f"{rank} sent {data} to {destination_process}")
 
 
 if rank == 4:
     data = comm.recv(source=0)
-    print("data received is = %s" % data)
+    print(f"{rank} sent {data} to {destination_process}")
 
 
 if rank == 8:
     data1 = comm.recv(source=1)
-    print("data1 received is = %s" % data1)
+    print(f"{rank} sent {data} to {destination_process}")
