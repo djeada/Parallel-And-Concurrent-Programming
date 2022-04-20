f7904fe25b5841c56f1a15f659cde21bfeddf262
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-20 17:29:10 +0200

    Update asyncio_future.py

--- src/python/asynchrony/asyncio_future.py
+++ src/python/asynchrony/asyncio_future.py
@@ -1,34 +1,41 @@
 import asyncio
 import sys
 
-
+# coroutine that sets the result directly in the future object
 async def sum_n_numbers(future, n):
     result = 0
     for i in range(n):
         result += i + 1
     future.set_result(f"Sum of n integers result = {result}")
 
-
-async def second_coroutine(future, n):
+# coroutine that returns the result like normal function
+async def second_coroutine(n):
     result = 1
     for i in range(n - 1):
         result *= i + 2
-    future.set_result(f"{n}! = {result}")
+        
+    return f"{n}! = {result}"
 
+def display_result(future):
+    print(future.result())
 
 if __name__ == "__main__":
 
     n = 5
 
     loop = asyncio.get_event_loop()
-    future1 = asyncio.Future()
-    future2 = asyncio.Future()
+    future = asyncio.Future()
+    
+    # create tasks from coroutines
+    task1 = loop.create_task(sum_n_numbers(future, n))
+    task2 = loop.create_task(second_coroutine(n))
 
-    tasks = (sum_n_numbers(future1, n), second_coroutine(future2, n))
-
+    # callback will be called when the task is completed
+    # task1.add_done_callback(display_result)
+    task2.add_done_callback(display_result)
+    
     loop.run_until_complete(asyncio.wait(tasks))
 
-    future1.result()
-    future2.result()
+    display_result(future)
 
     loop.close()
