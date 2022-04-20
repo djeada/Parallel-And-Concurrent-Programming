ef1017388294040b8c94e7c658d0d00879742965
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-21 00:30:09 +0200

    Update main.js

--- src/nodejs/multithreading/single_thread/main.js
+++ src/nodejs/multithreading/single_thread/main.js
@@ -1,18 +1,6 @@
-const fs = require('fs');
 const {Worker} = require("worker_threads");
 
 const worker_script_path = "./worker_script.js";
-
-const worker_script = ` 
-const {parentPort} = require("worker_threads");
-
-parentPort.on("message", (message) => {
-    parentPort.postMessage(\`worker thread process id: ${process.pid}\`);
-});
-`;
-
-// write worker script to file
-fs.writeFileSync(worker_script_path, worker_script);
 
 const worker = new Worker(worker_script_path, {
     workerData: "worker thread data"
