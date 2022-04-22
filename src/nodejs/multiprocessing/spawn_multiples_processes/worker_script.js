f4e47dc74e659413cc1b02ced1be2a01c2b63811
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-22 23:57:58 +0200

    Update worker_script.js

--- src/nodejs/multiprocessing/spawn_multiples_processes/worker_script.js
+++ src/nodejs/multiprocessing/spawn_multiples_processes/worker_script.js
@@ -1,5 +1,10 @@
 const {parentPort} = require("worker_threads");
 
+const args = process.argv.slice(2);
+const n = parseInt(args[0]);
+
+console.log(\`worker started with args: \${args}\`);
+
 parentPort.on("message", (message) => {
-    parentPort.postMessage(`worker thread process id: ${process.pid}`);
+    parentPort.postMessage(\`message received by ${process.pid}\`);
 });
