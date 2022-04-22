48cd5e46e4c405b5f79e12da73f569ca632b11b0
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-22 23:51:54 +0200

    Update main.js

--- src/nodejs/multiprocessing/spawn_multiples_processes/main.js
+++ src/nodejs/multiprocessing/spawn_multiples_processes/main.js
@@ -1,26 +1,34 @@
-const {Worker} = require("worker_threads");
+const spawn = require('child_process').spawn;
 
 const worker_script_path = "./worker_script.js";
 
-const worker = new Worker(worker_script_path, {
-    workerData: "worker thread data"
-});
+function spawn_new_process(worker_script_path, args) {
+    const worker = spawn(worker_script_path, args);
+    worker.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        worker.kill();
+    });
 
-worker.on("message", (message) => {
-    console.log(message);
-    worker.terminate();
-});
+    worker.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+        worker.kill();
+    });
 
+    worker.on('close', (code) => {
+        console.log(`child process with id ${worker.pid} exited with code ${code}`);
+    });
+}
 
-worker.on("error", (err) => {
-    console.log(err);
-    worker.terminate();
-});
-
+function spawn_n_processes(worker_script_path, args, n) {
+    for (let i = 0; i < n; i++) {
+        spawn_new_process(worker_script_path, args);
+    }
+}
 
 function main() {
-    console.log(`main thread process id: ${process.pid}`);
-    worker.postMessage("message from main thread");
+    const n = 10;
+    const args = ["--n", n.toString()];
+    spawn_n_processes(worker_script_path, args, n);
 }
 
 main();
