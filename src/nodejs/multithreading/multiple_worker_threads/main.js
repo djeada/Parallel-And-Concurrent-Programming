adfaf55528743fd5dd6a96cc82455c6f72f707c5
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-22 23:43:09 +0200

    Update main.js

--- src/nodejs/multithreading/multiple_worker_threads/main.js
+++ src/nodejs/multithreading/multiple_worker_threads/main.js
@@ -2,25 +2,38 @@ const {Worker} = require("worker_threads");
 
 const worker_script_path = "./worker_script.js";
 
-const worker = new Worker(worker_script_path, {
-    workerData: "worker thread data"
-});
 
-worker.on("message", (message) => {
-    console.log(message);
-    worker.terminate();
-});
+function spawn_worker(worker_script_path) {
+    const worker = new Worker(worker_script_path);
+    worker.on("message", (message) => {
+        console.log(`Received a message from thread with id: ${worker.threadId}`)
+        console.log(message);
+        worker.terminate();
+    });
+    worker.on("error", (error) => {
+        console.log(error);
+        worker.terminate();
+    });
+    worker.on("exit", (code) => {
+        console.log(`Worker ${worker.threadId} exited with code: ${code}`);
+    });
+    return worker;
+}
 
 
-worker.on("error", (err) => {
-    console.log(err);
-    worker.terminate();
-});
-
+function spawn_n_workers(n, worker_script_path) {
+    const workers = [];
+    for (let i = 0; i < n; i++) {
+        workers.push(spawn_worker(worker_script_path));
+    }
+    return workers;
+}
 
 function main() {
-    console.log(`main thread process id: ${process.pid}`);
-    worker.postMessage("message from main thread");
+    const workers = spawn_n_workers(10, worker_script_path);
+    workers.forEach((worker) => {
+        worker.postMessage("Hello from main thread");
+    });
 }
 
 main();
