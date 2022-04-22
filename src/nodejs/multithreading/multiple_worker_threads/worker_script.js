7c30e34b6b5948b07d67e65ec0f76bcf6c72a512
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2022-04-22 23:44:17 +0200

    Update worker_script.js

--- src/nodejs/multithreading/multiple_worker_threads/worker_script.js
+++ src/nodejs/multithreading/multiple_worker_threads/worker_script.js
@@ -1,5 +1,5 @@
 const {parentPort} = require("worker_threads");
 
 parentPort.on("message", (message) => {
-    parentPort.postMessage(`worker thread process id: ${process.pid}`);
+    parentPort.postMessage("Hello from worker thread");
 });
