69f60bb4d9d1f373ef11346d491fb0ef979560d1
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-13 23:26:02 +0200

    Update mutex.py

--- src/python/asynchrony/mutex.py
+++ src/python/asynchrony/mutex.py
@@ -1,24 +1,27 @@
 import asyncio
-import time
+import random
 
+async def critical_section(id):
+    print(f"Worker {id} is entering the critical section")
+    await asyncio.sleep(random.uniform(0.5, 2))
+    print(f"Worker {id} is leaving the critical section")
 
 async def foo(lock, id):
-    async with lock:
-        for _ in range(5):
-            print(f"Worker {id} is going to sleep")
-            await asyncio.sleep(1)
+    for _ in range(3):
+        async with lock:
+            await critical_section(id)
+        await asyncio.sleep(random.uniform(0.5, 1))
     print(f"End of worker {id} function")
-
 
 async def task_generator():
     lock = asyncio.Lock()
     n_workers = 3
-    await asyncio.wait([foo(lock, i) for i in range(n_workers)])
+    await asyncio.gather(*[foo(lock, i) for i in range(n_workers)])
 
+async def main():
+    await task_generator()
 
-lock = asyncio.Lock()
-loop = asyncio.get_event_loop()
-loop.run_until_complete(task_generator())
-loop.close()
+if __name__ == "__main__":
+    asyncio.run(main())
 
 print("The End")
