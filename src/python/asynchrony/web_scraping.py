5f82ee7e88748549baadc97dabfd8de55ec6141e
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-13 23:24:26 +0200

    Create web_scraping.py

---
+++ src/python/asynchrony/web_scraping.py
@@ -0,0 +1,25 @@
+import aiohttp
+import asyncio
+
+async def fetch(url, session):
+    async with session.get(url) as response:
+        content = await response.text()
+        print(f"Fetched content from {url} with length {len(content)}")
+
+async def fetch_all(urls):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch(url, session) for url in urls]
+        await asyncio.gather(*tasks)
+
+async def main():
+    urls = [
+        "https://httpbin.org/get",
+        "https://api.github.com",
+        "https://www.python.org",
+        "https://www.example.com",
+    ]
+
+    await fetch_all(urls)
+
+if __name__ == "__main__":
+    asyncio.run(main())
