bf9797693fbcd5a59a6226acdf0f1f6128a6719a
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-13 21:15:59 +0200

    Create producer_consumer.py

---
+++ src/python/multithreading/producer_consumer.py
@@ -0,0 +1,31 @@
+import threading
+import time
+
+pause_event = threading.Event()
+
+def long_function():
+    i = 0
+    while True:
+        if pause_event.is_set():
+            pause_event.wait()
+        print(f"Executing step {i}")
+        i += 1
+        time.sleep(1)
+
+def button_handler():
+    while True:
+        input("Press ENTER to toggle pause/resume:")
+        if pause_event.is_set():
+            pause_event.clear()
+        else:
+            pause_event.set()
+
+if __name__ == "__main__":
+    thread1 = threading.Thread(target=long_function)
+    thread2 = threading.Thread(target=button_handler)
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
