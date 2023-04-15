863fd42e1f87c8476c228b1a4da42de1cddd66dd
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-16 01:12:27 +0200

    Update 05_mutex.cpp

--- src/cpp/multithreading/05_mutex.cpp
+++ src/cpp/multithreading/05_mutex.cpp
@@ -1,37 +1,30 @@
+
 #include <iostream>
-#include <memory>
-#include <mutex>
 #include <thread>
 #include <vector>
+#include <mutex>
+#include <chrono>
 
-std::mutex mutex;
+std::mutex mtx;
 
 void foo(int id) {
-  mutex.lock(); // comment out this line of code to test what happens without
-                // the mutex
-  for (int i = 0; i < 100; i++) {
-    std::cout << "Thread id: " << id << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  }
-  mutex.unlock(); // comment out this line of code to test what happens without
-                  // the mutex
+    mtx.lock(); // comment out this line of code to test what happens without the mutex
+    for (int i = 0; i < 100; ++i) {
+        std::cout << "Thread id: " << id << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    }
+    mtx.unlock(); // comment out this line of code to test what happens without the mutex
 }
 
-auto main() -> int {
-  std::cout << "MAIN FUNCTION STARTS" << std::endl;
-
-  std::vector<std::unique_ptr<std::thread>> threads;
-
-  threads.reserve(4);
-  for (int i = 0; i < 4; i++) {
-    threads.emplace_back(std::make_unique<std::thread>(foo, i));
-  }
-
-  for (auto &thread : threads) {
-    thread->join();
-  }
-
-  std::cout << "THE END" << std::endl;
-
-  return 0;
+int main() {
+    std::cout << "MAIN FUNCTION STARTS" << std::endl;
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 4; ++i) {
+        threads.emplace_back(foo, i);
+    }
+    for (auto &thread : threads) {
+        thread.join();
+    }
+    std::cout << "THE END" << std::endl;
+    return 0;
 }
