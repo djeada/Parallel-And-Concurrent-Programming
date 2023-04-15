4d3eb0ce5e345b9ff2ef49fbcbd64dcb97c7e00f
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-16 01:25:36 +0200

    Update 05_mutex.js

--- src/js/multithreading/05_mutex.js
+++ src/js/multithreading/05_mutex.js
@@ -1,24 +1,59 @@
-import time
-from threading import Thread, Lock
+const { Worker, isMainThread, parentPort } = require('worker_threads');
 
-mutex = Lock()
+if (isMainThread) {
+  // Main thread
+  const numWorkers = 4;
+  const workers = [];
+  const sharedCounter = new SharedArrayBuffer(4);
+  const counterView = new Int32Array(sharedCounter);
+  counterView[0] = 0;
 
+  for (let i = 0; i < numWorkers; i++) {
+    const worker = new Worker(__filename); // Use the same file as the worker
+    worker.on('message', (message) => {
+      if (message.type === 'print') {
+        console.log(message.text);
+      }
+    });
+    worker.on('error', (error) => console.error(error));
+    worker.postMessage({ type: 'init', id: i, sharedCounter });
+    workers.push(worker);
+  }
 
-def foo(id):
-    mutex.acquire()  # comment out this line of code to test what happens without the mutex
-    for _ in range(100):
-        print("Thread id:", id)
-        time.sleep(0.05)
-    mutex.release()  # comment out this line of code to test what happens without the mutex
+  let currentWorker = 0;
+  function grantMutex() {
+    workers[currentWorker].postMessage({ type: 'grantMutex' });
+    currentWorker = (currentWorker + 1) % numWorkers;
+  }
 
+  workers.forEach((worker, index) => {
+    worker.on('message', (msg) => {
+      if (msg.type === 'releaseMutex') {
+        grantMutex();
+      }
+    });
+  });
 
-if __name__ == "__main__":
-    print("MAIN FUNCTION STARTS")
-    threads = []
-    for i in range(4):
-        threads.append(Thread(target=foo, args=(i,)))
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
-    print("THE END")
+  grantMutex(); // Grant mutex to the first worker
+} else {
+  // Worker thread
+  let id;
+  let sharedCounter;
+  let counterView;
+
+  parentPort.on('message', async (message) => {
+    if (message.type === 'init') {
+      id = message.id;
+      sharedCounter = message.sharedCounter;
+      counterView = new Int32Array(sharedCounter);
+      parentPort.postMessage({ type: 'requestMutex' });
+    } else if (message.type === 'grantMutex') {
+      for (let i = 0; i < 100; i++) {
+        parentPort.postMessage({ type: 'print', text: `Worker id: ${id}, Counter: ${counterView[0]}` });
+        Atomics.add(counterView, 0, 1);
+        await new Promise((resolve) => setTimeout(resolve, 50));
+      }
+      parentPort.postMessage({ type: 'releaseMutex' });
+    }
+  });
+}
