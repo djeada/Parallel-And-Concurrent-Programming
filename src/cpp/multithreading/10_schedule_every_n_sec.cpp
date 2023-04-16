99fe5ddda83524c4f12471fabba044de5c9e1be6
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 00:39:29 +0200

    Update 10_schedule_every_n_sec.cpp

--- src/cpp/multithreading/10_schedule_every_n_sec.cpp
+++ src/cpp/multithreading/10_schedule_every_n_sec.cpp
@@ -1,56 +1,49 @@
 #include <iostream>
 #include <thread>
-#include <vector>
+#include <atomic>
+#include <chrono>
+#include <functional>
 
-void merge(std::vector<int> &v, int left, int mid, int right) {
-  std::vector<int> temp(right - left + 1);
-  int i = left;
-  int j = mid + 1;
-  int k = 0;
-  while (i <= mid && j <= right) {
-    if (v[i] < v[j]) {
-      temp[k++] = v[i++];
-    } else {
-      temp[k++] = v[j++];
+class Scheduler {
+public:
+    Scheduler(int n, std::function<void()> function)
+        : flag(true), interval(n), task(std::move(function)) {
+        scheduler_thread = std::thread([this] {
+            while (flag) {
+                task();
+                std::this_thread::sleep_for(std::chrono::seconds(interval));
+            }
+        });
     }
-  }
-  while (i <= mid) {
-    temp[k++] = v[i++];
-  }
-  while (j <= right) {
-    temp[k++] = v[j++];
-  }
-  for (unsigned int i = 0; i < temp.size(); i++) {
-    v[left + i] = temp[i];
-  }
+
+    ~Scheduler() {
+        stop();
+    }
+
+    void stop() {
+        flag = false;
+        if (scheduler_thread.joinable()) {
+            scheduler_thread.join();
+        }
+    }
+
+private:
+    std::atomic<bool> flag;
+    int interval;
+    std::function<void()> task;
+    std::thread scheduler_thread;
+};
+
+void print_message() {
+    std::cout << "It works" << std::endl;
 }
 
-void merge_sort(std::vector<int> &v, int left, int right) {
-  if (left < right) {
-    int mid = (left + right) / 2;
-    merge_sort(v, left, mid);
-    merge_sort(v, mid + 1, right);
-    merge(v, left, mid, right);
-  }
-}
+int main() {
+    Scheduler scheduler(1, print_message);
 
-void merge_sort_thread(std::vector<int> &v, int left, int right) {
-  if (left < right) {
-    int mid = (left + right) / 2;
-    std::thread t1(merge_sort_thread, std::ref(v), left, mid);
-    std::thread t2(merge_sort_thread, std::ref(v), mid + 1, right);
-    t1.join();
-    t2.join();
-    merge(v, left, mid, right);
-  }
-}
+    // wait 10 seconds and stop the scheduler
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    scheduler.stop();
 
-auto main() -> int {
-  std::vector<int> v = {4, 2, 1, 5, 3, 6, 7, 8, 9, 0};
-  merge_sort_thread(v, 0, v.size() - 1);
-  for (const auto elem : v) {
-    std::cout << elem << " ";
-  }
-  std::cout << std::endl;
-  return 0;
+    return 0;
 }
