a422c8e1233c75357428d94be7880cbf28a9d5b1
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 00:13:51 +0200

    Update 08_fetch_parallel.cpp

--- src/cpp/multithreading/08_fetch_parallel.cpp
+++ src/cpp/multithreading/08_fetch_parallel.cpp
@@ -1,71 +1,71 @@
-#include <condition_variable>
 #include <iostream>
-#include <mutex>
-#include <thread>
+#include <string>
 #include <vector>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <curl/curl.h>
 
-class Semaphore {
-  std::mutex mutex;
-  std::condition_variable condition_;
-  unsigned long count = 0;
-
-public:
-  void release() {
-    std::lock_guard<decltype(mutex)> lock(mutex);
-    ++count;
-    condition_.notify_one();
-  }
-
-  void acquire() {
-    std::unique_lock<decltype(mutex)> lock(mutex);
-    while (count == 0u) {
-      condition_.wait(lock);
-    }
-    --count;
-  }
-
-  auto try_acquire() -> bool {
-    std::lock_guard<decltype(mutex)> lock(mutex);
-    if (count != 0u) {
-      --count;
-      return true;
-    }
-    return false;
-  }
+std::vector<std::string> urls = {
+    "https://www.example.com",
+    "https://www.example.org",
+    "https://www.example.net",
+    "https://www.example.edu",
 };
 
-Semaphore semaphore;
-int item = -1;
-
-void consumer() {
-  std::cout << "The begining of consumer function" << std::endl;
-  semaphore.acquire();
-  std::cout << "consumer got notified: the value is " << item << std::endl;
-  std::cout << "The end of consumer function" << std::endl;
+std::size_t writeFunction(void* contents, std::size_t size, std::size_t nmemb, void* userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
 }
 
-void producer() {
-  std::cout << "The begining of producer function" << std::endl;
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+std::pair<std::string, std::size_t> fetchUrl(const std::string& url) {
+    CURL* curl = curl_easy_init();
+    std::string fetchedContent;
 
-  item = 100;
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &fetchedContent);
+        curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+    }
 
-  std::cout << "producer send's the value " << item << std::endl;
-  semaphore.release();
-  std::cout << "The end of producer function" << std::endl;
+    return {url, fetchedContent.length()};
 }
 
-auto main() -> int {
+void concurrentFetch(const std::string& url) {
+    auto [fetchedUrl, contentLength] = fetchUrl(url);
+    std::cout << "Concurrent - URL: " << fetchedUrl << ", Content Length: " << contentLength << std::endl;
+}
 
-  std::vector<std::thread> threads;
+int main() {
+    curl_global_init(CURL_GLOBAL_DEFAULT);
 
-  threads.emplace_back(std::thread(producer));
-  threads.emplace_back(std::thread(consumer));
+    // Non-concurrent fetching
+    auto nonConcurrentStartTime = std::chrono::steady_clock::now();
+    for (const auto& url : urls) {
+        auto [fetchedUrl, contentLength] = fetchUrl(url);
+        std::cout << "Non-concurrent - URL: " << fetchedUrl << ", Content Length: " << contentLength << std::endl;
+    }
+    auto nonConcurrentEndTime = std::chrono::steady_clock::now();
+    auto nonConcurrentElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(nonConcurrentEndTime - nonConcurrentStartTime).count();
+    std::cout << "Non-concurrent time taken: " << nonConcurrentElapsed / 1000.0 << " seconds" << std::endl;
 
-  for (auto &thread : threads) {
-    thread.join();
-  }
+    // Concurrent fetching
+    auto concurrentStartTime = std::chrono::steady_clock::now();
+    std::vector<std::thread> threads;
+    for (const auto& url : urls) {
+        threads.emplace_back(concurrentFetch, url);
+    }
 
-  std::cout << "The end of main function" << std::endl;
-  return 0;
+    for (auto& thread : threads) {
+        thread.join();
+    }
+    auto concurrentEndTime = std::chrono::steady_clock::now();
+    auto concurrentElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(concurrentEndTime - concurrentStartTime).count();
+    std::cout << "Concurrent time taken: " << concurrentElapsed / 1000.0 << " seconds" << std::endl;
+
+    curl_global_cleanup();
+
+    return 0;
 }
