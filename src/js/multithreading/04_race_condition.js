450897988e61a7e33f5f28a8668936dcaef808a7
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-16 21:26:26 +0200

    Update 04_race_condition.js

--- src/js/multithreading/04_race_condition.js
+++ src/js/multithreading/04_race_condition.js
@@ -1,21 +1,56 @@
-let counter = 1;
+// main-worker.js
+const { Worker, isMainThread, parentPort } = require('worker_threads');
 
-function foo(multiplier) {
-  for (let i = 0; i < 10; i++) {
-    setTimeout(() => {
-      const oldValue = counter;
-      const newValue = oldValue * multiplier;
-      console.log(`The counter gets multiplied by ${multiplier}`);
-      counter = newValue;
-    }, i * 10 + Math.random() * 100);
-  }
+if (isMainThread) {
+  // Main thread
+  const numThreads = 5;
+  const sharedBuffer = new SharedArrayBuffer(8);
+  const counterView = new Int32Array(sharedBuffer, 0, 1);
+  const doneCounterView = new Int32Array(sharedBuffer, 4, 1);
+  counterView[0] = 1;
+
+  (async () => {
+    const workers = [];
+
+    for (let i = 0; i < numThreads; i++) {
+      const worker = new Worker(__filename); // Use the same file as the worker
+      worker.on('message', (message) => {
+        if (message.type === 'print') {
+          console.log(message.text);
+        } else if (message.type === 'done') {
+          doneCounterView[0]++;
+          if (doneCounterView[0] === numThreads) {
+            console.log(`The final value of COUNTER is: ${counterView[0]}`);
+          }
+        }
+      });
+      worker.on('error', (error) => console.error(error));
+      worker.postMessage({ type: 'init', id: i, sharedBuffer });
+      workers.push(worker);
+    }
+  })();
+} else {
+  // Worker thread
+  let id;
+  let sharedBuffer;
+  let counterView;
+
+  parentPort.on('message', async (message) => {
+    if (message.type === 'init') {
+      id = message.id;
+      sharedBuffer = message.sharedBuffer;
+      counterView = new Int32Array(sharedBuffer, 0, 1);
+
+      for (let i = 0; i < 10; i++) {
+        const localCounter = counterView[0];
+        const multipliedCounter = localCounter * (id + 1);
+        await new Promise((resolve) => setTimeout(resolve, 100)); // Add a delay
+        parentPort.postMessage({ type: 'print', text: `The COUNTER gets multiplied by ${id + 1}` });
+        counterView[0] = multipliedCounter;
+      }
+
+      parentPort.postMessage({ type: 'done' });
+      parentPort.close(); // Close the communication channel and allow the worker to exit gracefully
+    }
+  });
 }
-
-for (let i = 0; i < 3; i++) {
-  foo(i + 1);
-}
-
-// Wait for all tasks to finish, then print the final value of counter
-setTimeout(() => {
-  console.log(`The final value of counter is: ${counter}`);
-}, 2000);
