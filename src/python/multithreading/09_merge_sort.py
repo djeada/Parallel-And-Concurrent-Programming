855388deb3d0d1020dada7310f5571edd8ae854a
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 00:22:42 +0200

    Update 09_merge_sort.py

--- src/python/multithreading/09_merge_sort.py
+++ src/python/multithreading/09_merge_sort.py
@@ -1,40 +1,78 @@
-import concurrent.futures
+"""
+This script demonstrates the use of threading to perform a merge sort on a list of integers.
+The merge sort function is implemented both with and without threading, and the time taken
+for both methods is compared.
+"""
 
+import random
+import time
+from concurrent.futures import ThreadPoolExecutor
 
-def merge_sort_thread(arr):
-    def merge(arr_a, arr_b):
-        result = list()
-        index_a = index_b = 0
-
-        while len(arr_a) + len(arr_b) > index_a + index_b:
-
-            if len(arr_b) <= index_b or (
-                len(arr_a) > index_a and arr_a[index_a] < arr_b[index_b]
-            ):
-                result.append(arr_a[index_a])
-                index_a += 1
-
-            else:
-                result.append(arr_b[index_b])
-                index_b += 1
-
-        return result
-
-    n = len(arr)
-
-    if n < 2:
+def merge_sort(arr):
+    if len(arr) <= 1:
         return arr
 
-    # each instance should be in a separate thread
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_a = executor.submit(merge_sort_thread, arr[: n // 2])
-        arr_a = future_a.result()
-        future_b = executor.submit(merge_sort_thread, arr[n // 2 :])
-        arr_b = future_b.result()
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
 
-    return merge(arr_a, arr_b)
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
 
 
-array = [4, 2, 1, 5, 3, 6, 7, 8, 9, 0]
+def merge(left, right):
+    result = []
+    left_index = 0
+    right_index = 0
 
-print(merge_sort_thread(array))
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    if left_index < len(left):
+        result.extend(left[left_index:])
+    if right_index < len(right):
+        result.extend(right[right_index:])
+
+    return result
+
+
+def threaded_merge_sort(arr, depth=0, max_depth=2):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    if depth < max_depth:
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            left_future = executor.submit(threaded_merge_sort, left, depth+1, max_depth)
+            right_future = executor.submit(threaded_merge_sort, right, depth+1, max_depth)
+            left = left_future.result()
+            right = right_future.result()
+    else:
+        left = threaded_merge_sort(left, depth+1, max_depth)
+        right = threaded_merge_sort(right, depth+1, max_depth)
+
+    return merge(left, right)
+
+
+if __name__ == "__main__":
+    arr = [random.randint(1, 10000) for _ in range(10000)]
+
+    start_time = time.time()
+    sorted_arr = merge_sort(arr)
+    end_time = time.time()
+    print(f"Non-threaded merge sort time: {end_time - start_time:.5f} seconds")
+
+    start_time = time.time()
+    sorted_arr_threaded = threaded_merge_sort(arr)
+    end_time = time.time()
+    print(f"Threaded merge sort time: {end_time - start_time:.5f} seconds")
