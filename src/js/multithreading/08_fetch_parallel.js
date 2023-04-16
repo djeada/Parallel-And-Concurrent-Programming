a11549654aeeb718e3c1340621ecd5fea52d6c6a
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 00:11:46 +0200

    Update 08_fetch_parallel.js

--- src/js/multithreading/08_fetch_parallel.js
+++ src/js/multithreading/08_fetch_parallel.js
@@ -1,42 +1,63 @@
-import urllib.request
-import urllib.parse
-import urllib.error
-import pathlib
-import os
-import threading
-import concurrent.futures
-import time
-import urllib3
-import queue
+const axios = require('axios');
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
 
+const urls = [
+  'https://www.example.com',
+  'https://www.example.org',
+  'https://www.example.net',
+  'https://www.example.edu',
+];
 
-def read_url(url, queue):
-    try:
-        data = urllib.request.urlopen(url, None, 15).read()
-        print(f"Fetched {len(data)} from {url}")
-        queue.put(data)
-    except Exception as e:
-        print(e)
+async function fetchUrl(url) {
+  const response = await axios.get(url);
+  const contentLength = response.data.length;
+  return { url, contentLength };
+}
 
+async function nonConcurrentFetch() {
+  const nonConcurrentResults = {};
+  const startTime = Date.now();
 
-def fetch_parallel(urls):
-    result = queue.Queue()
-    threads = [threading.Thread(target=read_url, args=(url, result)) for url in urls]
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
-    return result
+  for (const url of urls) {
+    const { url: fetchedUrl, contentLength } = await fetchUrl(url);
+    nonConcurrentResults[fetchedUrl] = contentLength;
+    console.log(`Non-concurrent - URL: ${fetchedUrl}, Content Length: ${contentLength}`);
+  }
 
+  console.log(`Non-concurrent results: ${JSON.stringify(nonConcurrentResults)}`);
+  console.log(`Non-concurrent time taken: ${(Date.now() - startTime) / 1000} seconds`);
+}
 
-urls = (
-    "https://github.com/djeada/Od-C-do-Cpp#L-warto%C5%9Bci-i-r-warto%C5%9Bci",
-    "https://github.com/djeada/Od-C-do-Cpp#L-warto%C5%9Bci-i-r-warto%C5%9Bci",
-    "https://github.com/djeada/Od-C-do-Cpp#L-warto%C5%9Bci-i-r-warto%C5%9Bci",
-)
+if (isMainThread) {
+  (async () => {
+    // Non-concurrent fetching
+    await nonConcurrentFetch();
 
-fetched_sites = fetch_parallel(urls)
-result = list()
+    // Concurrent fetching
+    const startTime = Date.now();
+    const workers = [];
 
-while not fetched_sites.empty():
-    result.append(fetched_sites.get_nowait())
+    for (const url of urls) {
+      const worker = new Worker(__filename, { workerData: url });
+      worker.on('message', ({ url, contentLength }) => {
+        console.log(`Concurrent - URL: ${url}, Content Length: ${contentLength}`);
+      });
+      workers.push(worker);
+    }
+
+    for (const worker of workers) {
+      await new Promise((resolve) => {
+        worker.on('exit', () => {
+          resolve();
+        });
+      });
+    }
+
+    console.log(`Concurrent time taken: ${(Date.now() - startTime) / 1000} seconds`);
+  })();
+} else {
+  (async () => {
+    const { url, contentLength } = await fetchUrl(workerData);
+    parentPort.postMessage({ url, contentLength });
+  })();
+}
