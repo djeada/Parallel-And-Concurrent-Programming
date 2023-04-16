d2edf928bbbbcfa5e8e467b0290f3cf28a60952e
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 00:36:54 +0200

    Update 10_schedule_every_n_sec.py

--- src/python/multithreading/10_schedule_every_n_sec.py
+++ src/python/multithreading/10_schedule_every_n_sec.py
@@ -1,14 +1,27 @@
+"""
+A simple program to demonstrate a scheduler that runs a function every n seconds.
+The scheduler runs the function in a separate thread and can be stopped after a specified duration.
+"""
+
 import time
 from threading import Thread
 
 
 class Scheduler:
-
     """
-    Every n seconds, start executing the given function.
+    A scheduler class that repeatedly executes a given function every n seconds.
     """
 
     def __init__(self, n, function, *args, **kwargs):
+        """
+        Initialize the scheduler with the interval n, the function to execute, and any arguments or keyword arguments.
+
+        :param n: The interval in seconds between executions of the function.
+        :param function: The function to execute.
+        :param args: Positional arguments for the function.
+        :param kwargs: Keyword arguments for the function.
+        """
+
         def target():
             while self.flag:
                 function(*args, **kwargs)
@@ -20,15 +33,19 @@ class Scheduler:
 
     def stop(self):
         """
-        Stop the new function calls from being scheduled.
-        If the previous function has not yet completed, it will not be terminated.
+        Stop scheduling new function calls.
+        If the previous function call has not yet completed, it will not be terminated.
         """
         self.flag = False
         self.thread.join()
 
 
-scheduler = Scheduler(1, print, "It works")
+def sample_function(message):
+    print(message)
 
-# wait 10 seconds and stop the scheduler
-time.sleep(10)
-scheduler.stop()
+if __name__ == "__main__":
+  scheduler = Scheduler(1, sample_function, "It works!")
+  
+  # Wait 10 seconds and stop the scheduler
+  time.sleep(10)
+  scheduler.stop()
