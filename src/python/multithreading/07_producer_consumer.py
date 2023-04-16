4cd64abb4826c09ca086662f3dc2ee31408fd1d3
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-16 21:48:28 +0200

    Update 07_producer_consumer.py

--- src/python/multithreading/07_producer_consumer.py
+++ src/python/multithreading/07_producer_consumer.py
@@ -1,34 +1,53 @@
-import threading
+"""
+This script demonstrates the producer-consumer pattern using Python threads.
+It simulates a scenario where producers create items and put them into a shared buffer,
+and consumers take items from the buffer and process them.
+
+The shared buffer is represented by a Python Queue, which is thread-safe by design.
+"""
+
+import random
 import time
+from queue import Queue
+from threading import Thread
 
-pause_event = threading.Event()
+
+def producer(buffer, id):
+    for _ in range(10):
+        item = random.randint(1, 100)
+        buffer.put(item)
+        print(f"Producer {id} produced item {item}")
+        time.sleep(random.uniform(0.1, 0.5))
 
 
-def long_function():
-    i = 0
+def consumer(buffer, id):
     while True:
-        if pause_event.is_set():
-            pause_event.wait()
-        print(f"Executing step {i}")
-        i += 1
-        time.sleep(1)
-
-
-def button_handler():
-    while True:
-        input("Press ENTER to toggle pause/resume:")
-        if pause_event.is_set():
-            pause_event.clear()
-        else:
-            pause_event.set()
+        item = buffer.get()
+        if item is None:
+            break
+        print(f"Consumer {id} consumed item {item}")
+        time.sleep(random.uniform(0.1, 0.5))
 
 
 if __name__ == "__main__":
-    thread1 = threading.Thread(target=long_function)
-    thread2 = threading.Thread(target=button_handler)
+    buffer = Queue(maxsize=10)
+    num_producers = 3
+    num_consumers = 3
 
-    thread1.start()
-    thread2.start()
+    producers = [Thread(target=producer, args=(buffer, i + 1)) for i in range(num_producers)]
+    consumers = [Thread(target=consumer, args=(buffer, i + 1)) for i in range(num_consumers)]
 
-    thread1.join()
-    thread2.join()
+    for producer in producers:
+        producer.start()
+
+    for consumer in consumers:
+        consumer.start()
+
+    for producer in producers:
+        producer.join()
+
+    for _ in range(num_consumers):
+        buffer.put(None)
+
+    for consumer in consumers:
+        consumer.join()
