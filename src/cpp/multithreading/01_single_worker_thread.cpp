bcdc3fb4889af85af22de3f55fbc2ebb25cbe5e0
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 22:11:19 +0200

    Update 01_single_worker_thread.cpp

--- src/cpp/multithreading/01_single_worker_thread.cpp
+++ src/cpp/multithreading/01_single_worker_thread.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 #include <thread>
-#include <unistd.h> // getpid() definition
+#include <string>
+#include <unistd.h>
+#include <sys/syscall.h>
 
 void foo() {
-  std::cout << "Worker function." << std::endl;
-  std::cout << "Worker function id: " << getpid() << std::endl;
-  std::cout << "Worker thread id: " << std::this_thread::get_id() << std::endl;
+    std::cout << "Worker function" << std::endl;
+    std::cout << "Worker function process id: " << getpid() << std::endl;
+    std::cout << "Worker thread id: " << std::this_thread::get_id() << std::endl;
+    std::cout << "Worker thread name: Example_thread" << std::endl;
 }
 
-auto main() -> int {
-  std::cout << "Main function id: " << getpid() << std::endl;
-  std::cout << "Main thread id: " << std::this_thread::get_id() << std::endl;
+int main() {
+    std::cout << "Main function process id: " << getpid() << std::endl;
+    std::cout << "Main thread id: " << std::this_thread::get_id() << std::endl;
+    std::cout << "Main thread name: main" << std::endl;
 
-  std::thread thread(foo);
+    std::thread worker_thread(foo);
 
   // if we use join main thread will wait for that thread to finish
-  thread.join();
+    worker_thread.join();
 
   // other option is to detach the thread and then it will proceede freely on
-  // it's own --daemon process thread.detach();
+  // it's own --daemon process worker_thread.detach();
 
   // you can join and detach only once
 
-  std::cout << std::boolalpha << thread.joinable() << std::endl;
+  std::cout << std::boolalpha << worker_thread.joinable() << std::endl;
 
   return 0;
 }
