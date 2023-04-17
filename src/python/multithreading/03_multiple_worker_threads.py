e1b13fdb04e86bf6f82f1be70494ffde301ae871
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 22:22:47 +0200

    Update 03_multiple_worker_threads.py

--- src/python/multithreading/03_multiple_worker_threads.py
+++ src/python/multithreading/03_multiple_worker_threads.py
@@ -1,18 +1,21 @@
+"""
+This script demonstrates the creation of multiple threads that execute different functions concurrently.
+It creates two sets of threads, one set running the 'foo' function and the other set running the 'bar' function.
+Each function has a different execution time, and each thread is given an ID to track its progress.
+"""
+
 import time
 from threading import Thread
 
-
 def foo(id):
-    print("Function foo() starts, Thread id: ", id)
+    print(f"Function foo() starts, Thread id: {id}")
     time.sleep(1)
-    print("Function foo() finishes, Thread id: ", id)
-
+    print(f"Function foo() finishes, Thread id: {id}")
 
 def bar(id):
-    print("Function bar() starts, Thread id: ", id)
+    print(f"Function bar() starts, Thread id: {id}")
     time.sleep(0.1)
-    print("Function bar() finishes, Thread id: ", id)
-
+    print(f"Function bar() finishes, Thread id: {id}")
 
 if __name__ == "__main__":
     print("Main thread starts")
