0d2fd3ed20866b9966f5e5ad96f3435e11c6bf5a
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 22:24:10 +0200

    Update 03_multiple_worker_threads.cpp

--- src/cpp/multithreading/03_multiple_worker_threads.cpp
+++ src/cpp/multithreading/03_multiple_worker_threads.cpp
@@ -3,36 +3,35 @@
 #include <vector>
 
 void foo(int id) {
-  std::cout << "Function foo() starts, Thread id: " << id << std::endl;
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  std::cout << "Function foo() finishes, Thread id: " << id << std::endl;
+    std::cout << "Function foo() starts, Thread id: " << id << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Function foo() finishes, Thread id: " << id << std::endl;
 }
 
 void bar(int id) {
-  std::cout << "Function bar() starts, Thread id: " << id << std::endl;
-  std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  std::cout << "Function bar() finishes, Thread id: " << id << std::endl;
+    std::cout << "Function bar() starts, Thread id: " << id << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout << "Function bar() finishes, Thread id: " << id << std::endl;
 }
 
-auto main() -> int {
+int main() {
+    std::cout << "Main thread starts" << std::endl;
 
-  std::cout << "Main thread starts" << std::endl;
+    std::vector<std::thread> threads;
 
-  std::vector<std::thread> threads;
-  threads.reserve(5);
-  for (int i = 0; i < 5; i++) {
-    threads.emplace_back(foo, i);
-  }
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back(foo, i);
+    }
 
-  for (int i = 0; i < 5; i++) {
-    threads.emplace_back(bar, i);
-  }
+    for (int i = 0; i < 5; ++i) {
+        threads.emplace_back(bar, i);
+    }
 
-  for (auto &thread : threads) {
-    thread.join();
-  }
+    for (auto &thread : threads) {
+        thread.join();
+    }
 
-  std::cout << "Main thread finishes" << std::endl;
+    std::cout << "Main thread finishes" << std::endl;
 
-  return 0;
+    return 0;
 }
