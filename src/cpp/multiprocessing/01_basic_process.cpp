b54a8e46717874557e5ca7a2eae41b54f72095d8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 22:29:59 +0200

    Update 01_basic_process.cpp

--- src/cpp/multiprocessing/01_basic_process.cpp
+++ src/cpp/multiprocessing/01_basic_process.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
-#include <sys/wait.h>
 #include <thread>
-#include <unistd.h> // getpid() definition
+#include <mutex>
+#include <sys/types.h>
+#include <unistd.h>
 
 void foo() {
-  std::cout << "Worker function." << std::endl;
-  std::cout << "Worker function id: " << getpid() << std::endl;
-  std::cout << "Worker thread id: " << std::this_thread::get_id() << std::endl;
+    std::cout << "Worker function" << std::endl;
+    std::cout << "Worker function process id: " << getpid() << std::endl;
+    std::cout << "Worker thread id: " << std::this_thread::get_id() << std::endl;
+    std::cout << "Worker function finished" << std::endl;
 }
 
-auto main() -> int {
-  std::cout << "Main function id: " << getpid() << std::endl;
-  std::cout << "Main thread id: " << std::this_thread::get_id() << std::endl;
+int main() {
+    std::cout << "Main function" << std::endl;
+    std::cout << "Main function process id: " << getpid() << std::endl;
+    std::cout << "Main thread id: " << std::this_thread::get_id() << std::endl;
 
-  // create a new process with fork
-  auto pid = fork();
-  if (pid == 0) {
-    // child process
-    foo();
-  } else {
-    // parent process
-    std::cout << "Parents waits for its child" << std::endl;
-    wait(nullptr);
-    std::cout << "Parents finished waiting" << std::endl;
-  }
+    pid_t pid = fork();
 
-  return 0;
+    if (pid == 0) {
+        // Child process
+        foo();
+    } else if (pid > 0) {
+        // Parent process
+        waitpid(pid, nullptr, 0);
+        std::cout << "Main function finished" << std::endl;
+    } else {
+        std::cerr << "Fork failed" << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
