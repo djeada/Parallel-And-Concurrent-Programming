afec5ce6fea7fe16630ac22b77f348f91c977bc2
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 23:13:54 +0200

    Update 02_multiple_processes.cpp

--- src/cpp/multiprocessing/02_multiple_processes.cpp
+++ src/cpp/multiprocessing/02_multiple_processes.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <chrono>
+#include <thread>
 
-const int NUM_PROCESSES = 5;
-
-void printMessage(const int &processNum) {
-  std::cout << "Process #" << processNum << " with PID " << getpid()
-            << " is running." << std::endl;
+void foo() {
+    std::cout << "Function foo starts" << std::endl;
+    std::cout << "Worker function process id: " << getpid() << std::endl;
+    std::cout << "Worker thread id: " << pthread_self() << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Function foo finishes" << std::endl;
 }
 
-auto main() -> int {
+void bar() {
+    std::cout << "Function bar starts" << std::endl;
+    std::cout << "Worker function process id: " << getpid() << std::endl;
+    std::cout << "Worker thread id: " << pthread_self() << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::cout << "Function bar finishes" << std::endl;
+}
 
-  printMessage(0);
+int main() {
+    std::cout << "Main function" << std::endl;
+    std::cout << "Main function process id: " << getpid() << std::endl;
+    std::cout << "Main thread id: " << pthread_self() << std::endl;
 
-  for (int j = 1; j <= NUM_PROCESSES; j++) {
-    auto pid = fork();
-    if (pid < 0) {
-      std::cerr << "Fork failed" << std::endl;
-      exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-      printMessage(j);
-      exit(EXIT_SUCCESS);
+    pid_t pid = fork();
+
+    if (pid == 0) { // Child process
+        foo();
+    } else if (pid > 0) { // Parent process
+        pid_t pid2 = fork();
+        if (pid2 == 0) { // Second child process
+            bar();
+        } else if (pid2 > 0) { // Parent process
+            int status;
+            waitpid(pid, &status, 0);
+            waitpid(pid2, &status, 0);
+            std::cout << "Main function finished" << std::endl;
+        } else {
+            std::cerr << "Error: Failed to create the second child process" << std::endl;
+            return 1;
+        }
+    } else {
+        std::cerr << "Error: Failed to create the first child process" << std::endl;
+        return 1;
     }
-  }
 
-  int status = 0;
-  for (int i = 0; i < NUM_PROCESSES; i++) {
-    auto pid = wait(&status);
-    std::cout << "Process " << pid << " exited with status " << status
-              << std::endl;
-  }
-
-  return 0;
+    return 0;
 }
