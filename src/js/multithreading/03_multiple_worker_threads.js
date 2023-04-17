04166c245a2ce9ab733fb67614aa58ba872a09de
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-17 22:25:32 +0200

    Update 03_multiple_worker_threads.js

--- src/js/multithreading/03_multiple_worker_threads.js
+++ src/js/multithreading/03_multiple_worker_threads.js
@@ -1,62 +1,43 @@
-const {
-  Worker,
-  isMainThread,
-  parentPort,
-  workerData,
-} = require("worker_threads");
+const { Thread, Worker, isMainThread, parentPort, workerData } = require("worker_threads");
 
 function foo(id) {
-  console.log(`Function foo() starts, Thread id: ${id}`);
+  console.log("Function foo() starts, Thread id: ", id);
   setTimeout(() => {
-    console.log(`Function foo() finishes, Thread id: ${id}`);
-    parentPort.postMessage("done");
+    console.log("Function foo() finishes, Thread id: ", id);
   }, 1000);
 }
 
 function bar(id) {
-  console.log(`Function bar() starts, Thread id: ${id}`);
+  console.log("Function bar() starts, Thread id: ", id);
   setTimeout(() => {
-    console.log(`Function bar() finishes, Thread id: ${id}`);
-    parentPort.postMessage("done");
+    console.log("Function bar() finishes, Thread id: ", id);
   }, 100);
 }
 
-function main() {
+if (isMainThread) {
   console.log("Main thread starts");
 
   const threads = [];
+
   for (let i = 0; i < 5; i++) {
-    threads.push(
-      new Worker(__filename, {
-        workerData: { func: foo.toString(), args: [i] },
-      })
-    );
+    threads.push(new Worker(__filename, { workerData: { target: foo, args: [i] } }));
   }
 
   for (let i = 0; i < 5; i++) {
-    threads.push(
-      new Worker(__filename, {
-        workerData: { func: bar.toString(), args: [i] },
-      })
-    );
+    threads.push(new Worker(__filename, { workerData: { target: bar, args: [i] } }));
   }
 
   let finishedThreads = 0;
-  threads.forEach((thread) => {
-    thread.on("message", (message) => {
-      if (message === "done") {
-        finishedThreads++;
-        if (finishedThreads === threads.length) {
-          console.log("Main thread finishes");
-        }
+
+  for (const thread of threads) {
+    thread.on("exit", () => {
+      finishedThreads++;
+      if (finishedThreads === threads.length) {
+        console.log("Main thread finishes");
       }
     });
-  });
-}
-
-if (isMainThread) {
-  main();
+  }
 } else {
-  const func = eval(`(${workerData.func})`);
-  func(...workerData.args);
+  const { target, args } = workerData;
+  target(...args);
 }
