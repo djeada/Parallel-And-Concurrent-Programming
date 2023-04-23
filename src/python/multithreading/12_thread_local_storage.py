14cae397408fde52c940937e93a9f4339a686bc8
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-23 20:52:19 +0200

    Update 12_thread_local_storage.py

--- src/python/multithreading/12_thread_local_storage.py
+++ src/python/multithreading/12_thread_local_storage.py
@@ -1,7 +1,22 @@
 """
 This script demonstrates the use of threading.local() to implement
-Thread Local Storage (TLS) in Python. Each worker thread sets and reads
-thread-local data, which is independent of data in other threads.
+Thread Local Storage (TLS) in Python. Thread Local Storage (TLS) allows each
+thread to have its own private data that is independent of data in other threads.
+
+In this script, each worker thread sets and reads its own thread-local data.
+The main thread creates multiple worker threads, and each worker thread stores
+its thread_id in the thread-local data. Then, the worker performs a computation
+(simulated by some_work()) that modifies the thread-local data. Finally, each
+worker thread reads and prints its thread-local data.
+
+There are other ways to implement TLS, such as using a global dictionary with
+thread identifiers as keys, but using threading.local() provides a more
+convenient and efficient way to manage thread-local data.
+
+The threading.local() class used in this script creates an instance with
+attributes that are unique for each thread. When a thread accesses the
+thread-local data, it sees only its own data and is unaware of the data in other
+threads.
 """
 
 import threading
