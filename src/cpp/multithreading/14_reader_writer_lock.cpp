07a792e983e0540abb1e4b6833941df30c02cf0c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-23 21:15:08 +0200

    Update 14_reader_writer_lock.cpp

--- src/cpp/multithreading/14_reader_writer_lock.cpp
+++ src/cpp/multithreading/14_reader_writer_lock.cpp
@@ -1,84 +1,89 @@
-#include <chrono>
 #include <iostream>
-#include <mutex>
-#include <random>
-#include <shared_mutex>
-#include <thread>
 #include <vector>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <random>
 
 class SharedData {
 public:
-  void read(int thread_id) {
-    std::unique_lock<std::mutex> reader_lock(readers_mutex);
-    ++readers;
-    if (readers == 1) {
-      data_mutex.lock();
+    void read(int thread_id) {
+        {
+            std::unique_lock<std::mutex> lock(readers_mutex);
+            ++readers;
+            if (readers == 1) {
+                data_mutex.lock();
+            }
+        }
+
+        std::cout << "Reader " << thread_id << " reads data: " << data << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(random_duration(500, 1000)));
+
+        {
+            std::unique_lock<std::mutex> lock(readers_mutex);
+            --readers;
+            if (readers == 0) {
+                data_mutex.unlock();
+            }
+        }
     }
-    reader_lock.unlock();
 
-    std::cout << "Reader " << thread_id << " reads data: " << data << std::endl;
-    std::this_thread::sleep_for(
-        std::chrono::milliseconds(rand() % 1000 + 500)); // Simulate some work
-
-    reader_lock.lock();
-    --readers;
-    if (readers == 0) {
-      data_mutex.unlock();
+    void write(int thread_id) {
+        std::unique_lock<std::mutex> lock(data_mutex);
+        std::cout << "Writer " << thread_id << " is writing..." << std::endl;
+        ++data;
+        std::this_thread::sleep_for(std::chrono::milliseconds(random_duration(500, 1000)));
+        std::cout << "Writer " << thread_id << " finished writing. New data: " << data << std::endl;
     }
-  }
 
-  void write(int thread_id) {
-    std::unique_lock<std::shared_mutex> lock(data_mutex);
-    std::cout << "Writer " << thread_id << " is writing..." << std::endl;
-    ++data;
-    std::this_thread::sleep_for(
-        std::chrono::milliseconds(rand() % 1000 + 500)); // Simulate some work
-    std::cout << "Writer " << thread_id
-              << " finished writing. New data: " << data << std::endl;
-  }
-
+    int random_duration(int min, int max) {
+        std::random_device rd;
+        std::mt19937 mt(rd());
+        std::uniform_int_distribution<int> dist(min, max);
+        return dist(mt);
+    }
 private:
-  int data = 0;
-  int readers = 0;
-  std::mutex readers_mutex;
-  std::shared_mutex data_mutex;
+
+    int data = 0;
+    std::mutex data_mutex;
+    int readers = 0;
+    std::mutex readers_mutex;
 };
 
-void reader(SharedData &shared_data, int thread_id) {
-  while (true) {
-    shared_data.read(thread_id);
-    std::this_thread::sleep_for(
-        std::chrono::milliseconds(rand() % 1000 + 1000));
-  }
+void reader(SharedData& shared_data, int thread_id) {
+    while (true) {
+        shared_data.read(thread_id);
+        std::this_thread::sleep_for(std::chrono::milliseconds(shared_data.random_duration(1000, 2000)));
+    }
 }
 
-void writer(SharedData &shared_data, int thread_id) {
-  while (true) {
-    shared_data.write(thread_id);
-    std::this_thread::sleep_for(
-        std::chrono::milliseconds(rand() % 1000 + 1000));
-  }
+void writer(SharedData& shared_data, int thread_id) {
+    while (true) {
+        shared_data.write(thread_id);
+        std::this_thread::sleep_for(std::chrono::milliseconds(shared_data.random_duration(1000, 2000)));
+    }
 }
+
 
 int main() {
-  SharedData shared_data;
+    SharedData shared_data;
 
-  int num_readers = 3;
-  int num_writers = 2;
+    const int num_readers = 3;
+    const int num_writers = 2;
 
-  std::vector<std::thread> threads;
+    std::vector<std::thread> threads;
 
-  for (int i = 0; i < num_readers; ++i) {
-    threads.emplace_back(reader, std::ref(shared_data), i);
-  }
+    for (int i = 0; i < num_readers; ++i) {
+        threads.emplace_back(reader, std::ref(shared_data), i);
+    }
 
-  for (int i = 0; i < num_writers; ++i) {
-    threads.emplace_back(writer, std::ref(shared_data), i);
-  }
+    for (int i = 0; i < num_writers; ++i) {
+        threads.emplace_back(writer, std::ref(shared_data), i);
+    }
 
-  for (auto &t : threads) {
-    t.join();
-  }
+    for (auto& thread : threads) {
+        thread.join();
+    }
 
-  return 0;
+    return 0;
 }
