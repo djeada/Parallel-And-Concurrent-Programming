5baa89709a2449c8a96456d8d35b389d186912cb
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-24 00:04:16 +0200

    Update 06_pipe_communication.cpp

--- src/cpp/multiprocessing/06_pipe_communication.cpp
+++ src/cpp/multiprocessing/06_pipe_communication.cpp
@@ -1,28 +1,70 @@
-#include <fcntl.h>
+/*
+This program demonstrates the use of fork() to create multiple processes and pipes to establish communication
+between them in C++. A pipe allows for safe inter-process communication by providing a simple way to pass data
+between processes. In this example, we create a producer process that generates data and writes it into the pipe,
+and a consumer process that reads the data from the pipe.
+
+When working with multiple processes, it is important to ensure that data is safely shared between them.
+Pipes provide a way to achieve this. They can be used to send data between a producer and a consumer process.
+The producer generates data and writes it into the pipe, while the consumer reads the data from the pipe and
+processes it. This pattern is useful when the producer and consumer processes work at different rates or when
+you want to separate data generation from data processing.
+*/
+
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+#include <ctime>
 
-void spawnNumbers(int fd[2]) {
-  int i = 0;
-  while (i < 10) {
-    write(fd[1], &i, sizeof(i));
-    i++;
-  }
+void producer(int pipe_fd) {
+    srand(time(NULL));
+    for (int i = 0; i < 5; ++i) {
+        int item = rand() % 10 + 1;
+        std::cout << "Producer: Adding " << item << " to the pipe" << std::endl;
+        write(pipe_fd, &item, sizeof(item));
+        usleep((rand() % 500000) + 500000);
+    }
+    close(pipe_fd);
 }
 
-auto main() -> int {
-  int fd[2];
-  pipe2(fd, O_CLOEXEC);
-  pid_t pid = fork();
-  if (pid == 0) {
-    spawnNumbers(fd);
-  } else {
-    int i = 0;
-    while (i < 10) {
-      read(fd[0], &i, sizeof(i));
-      std::cout << i << std::endl;
-      i++;
+void consumer(int pipe_fd) {
+    int item;
+    ssize_t bytesRead;
+    while ((bytesRead = read(pipe_fd, &item, sizeof(item))) > 0) {
+        std::cout << "Consumer: Processing " << item << std::endl;
+        usleep((rand() % 500000) + 500000);
     }
-  }
-  return 0;
+    close(pipe_fd);
+}
+
+int main() {
+    int pipe_fd[2];
+
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+        // Child process (Consumer)
+        close(pipe_fd[1]);
+        consumer(pipe_fd[0]);
+        exit(EXIT_SUCCESS);
+    } else {
+        // Parent process (Producer)
+        close(pipe_fd[0]);
+        producer(pipe_fd[1]);
+
+        // Wait for the child process to finish
+        wait(NULL);
+    }
+
+    return 0;
 }
