5f8750f7adb4de2159a0b6027c0a9cb50eec4575
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-23 21:07:44 +0200

    Update 13_thread_pool.cpp

--- src/cpp/multithreading/13_thread_pool.cpp
+++ src/cpp/multithreading/13_thread_pool.cpp
@@ -1,95 +1,78 @@
-#include <atomic>
-#include <chrono>
-#include <condition_variable>
-#include <functional>
-#include <future>
 #include <iostream>
-#include <mutex>
-#include <queue>
-#include <random>
-#include <thread>
 #include <vector>
+#include <thread>
+#include <future>
+#include <chrono>
+#include <random>
+#include <deque>
+#include <mutex>
 
-class ThreadPool {
-public:
-  ThreadPool(size_t num_threads) {
-    for (size_t i = 0; i < num_threads; ++i) {
-      threads.emplace_back([this] {
-        while (true) {
-          std::unique_lock<std::mutex> lock(queue_mutex);
-          condition.wait(lock, [this] { return !tasks.empty() || stop; });
-
-          if (stop && tasks.empty()) {
-            break;
-          }
-
-          auto task = std::move(tasks.front());
-          tasks.pop();
-          lock.unlock();
-          task();
-        }
-      });
-    }
-  }
-
-  template <class F, class... Args>
-  auto enqueue(F &&f, Args &&...args)
-      -> std::future<typename std::result_of<F(Args...)>::type> {
-    using return_type = typename std::result_of<F(Args...)>::type;
-
-    auto task = std::make_shared<std::packaged_task<return_type()>>(
-        std::bind(std::forward<F>(f), std::forward<Args>(args)...));
-
-    std::future<return_type> result = task->get_future();
-    {
-      std::unique_lock<std::mutex> lock(queue_mutex);
-      if (stop) {
-        throw std::runtime_error("enqueue on stopped ThreadPool");
-      }
-      tasks.emplace([task]() { (*task)(); });
-    }
-    condition.notify_one();
+int worker(int task_id) {
+    std::cout << "Task " << task_id << " is starting..." << std::endl;
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> dist(1, 3);
+    std::this_thread::sleep_for(std::chrono::seconds(dist(mt))); // Simulate some work
+    int result = task_id * 2;
+    std::cout << "Task " << task_id << " is finished. Result: " << result << std::endl;
     return result;
-  }
-
-  ~ThreadPool() {
-    {
-      std::unique_lock<std::mutex> lock(queue_mutex);
-      stop = true;
-    }
-    condition.notify_all();
-    for (std::thread &t : threads) {
-      t.join();
-    }
-  }
-
-private:
-  std::vector<std::thread> threads;
-  std::queue<std::function<void()>> tasks;
-  std::mutex queue_mutex;
-  std::condition_variable condition;
-  std::atomic<bool> stop{false};
-};
-
-void my_function(int i) {
-  std::cout << "Task " << i << " started." << std::endl;
-  std::this_thread::sleep_for(
-      std::chrono::milliseconds(rand() % 1000 + 500)); // Simulate some work
-  std::cout << "Task " << i << " finished." << std::endl;
 }
 
 int main() {
-  ThreadPool thread_pool(4);
+    const int num_tasks = 10;
+    const int num_workers = 3;
+    std::vector<std::future<int>> futures;
 
-  std::vector<std::future<void>> futures;
-  for (int i = 0; i < 10; ++i) {
-    futures.emplace_back(thread_pool.enqueue(my_function, i));
-  }
+    // Create a thread pool
+    {
+        std::vector<std::thread> threads;
+        std::deque<int> task_ids;
+        std::mutex task_ids_mutex;
+        std::vector<std::packaged_task<int(int)>> tasks(num_tasks);
 
-  for (auto &future : futures) {
-    future.wait();
-  }
+        // Initialize tasks and task_ids
+        for (int i = 0; i < num_tasks; ++i) {
+            tasks[i] = std::packaged_task<int(int)>(worker);
+            task_ids.push_back(i);
+        }
 
-  std::cout << "Main thread finished." << std::endl;
-  return 0;
+        // Worker function
+        auto worker_fn = [&]() {
+            while (true) {
+                int task_id;
+                {
+                    std::lock_guard<std::mutex> lock(task_ids_mutex);
+                    if (task_ids.empty()) {
+                        break;
+                    }
+                    task_id = task_ids.front();
+                    task_ids.pop_front();
+                }
+                tasks[task_id].operator()(task_id);
+            }
+        };
+
+        // Launch worker threads
+        for (int i = 0; i < num_workers; ++i) {
+            threads.emplace_back(worker_fn);
+        }
+
+        // Join worker threads
+        for (auto& thread : threads) {
+            thread.join();
+        }
+
+        // Retrieve futures
+        for (auto& task : tasks) {
+            futures.push_back(task.get_future());
+        }
+    }
+
+    // Collect the results as the tasks complete
+    for (int i = 0; i < num_tasks; ++i) {
+        int result = futures[i].get();
+        std::cout << "Task " << i << " result collected: " << result << std::endl;
+    }
+
+    return 0;
 }
