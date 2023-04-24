fc35bad3c67056492e79e003958148691d2aed80
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-25 00:16:27 +0200

    Update 07_shared_value.js

--- src/js/multiprocessing/07_shared_value.js
+++ src/js/multiprocessing/07_shared_value.js
@@ -1,24 +1,39 @@
-import multiprocessing
+const { fork } = require('child_process');
 
+if (process.argv[2] === 'child') {
+  process.on('message', (message) => {
+    if (message.action === 'increment') {
+      process.send({ action: 'increment' });
+    } else if (message.action === 'decrement') {
+      process.send({ action: 'decrement' });
+    }
+  });
+} else {
+  const child1 = fork(__filename, ['child']);
+  const child2 = fork(__filename, ['child']);
 
-def spawn_numbers(pipe):
-    input_pipe, _ = pipe
-    for i in range(10):
-        input_pipe.send(i)
-    input_pipe.close()
+  let sharedValue = 0;
 
+  child1.on('message', (message) => {
+    if (message.action === 'increment') {
+      sharedValue++;
+      console.log(`Shared value incremented by child1: ${sharedValue}`);
+      setTimeout(() => {
+        child1.send({ action: 'increment' });
+      }, 1000);
+    }
+  });
 
-if __name__ == "__main__":
+  child2.on('message', (message) => {
+    if (message.action === 'decrement') {
+      sharedValue--;
+      console.log(`Shared value decremented by child2: ${sharedValue}`);
+      setTimeout(() => {
+        child2.send({ action: 'decrement' });
+      }, 1000);
+    }
+  });
 
-    pipe = multiprocessing.Pipe(True)
-    process = multiprocessing.Process(target=spawn_numbers, args=(pipe,))
-    process.start()
-    process.join()
-
-    _, output_pipe = pipe
-
-    try:
-        while 1:
-            print(output_pipe.recv())
-    except EOFError:
-        print("End")
+  child1.send({ action: 'increment' });
+  child2.send({ action: 'decrement' });
+}
