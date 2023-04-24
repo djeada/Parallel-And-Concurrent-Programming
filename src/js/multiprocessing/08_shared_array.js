f1a4299381ae2681cb97e2a86c4252ba403393b7
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-25 00:17:35 +0200

    Update 08_shared_array.js

--- src/js/multiprocessing/08_shared_array.js
+++ src/js/multiprocessing/08_shared_array.js
@@ -1,24 +1,39 @@
-import multiprocessing
+const { fork } = require('child_process');
 
+if (process.argv[2] === 'child') {
+  process.on('message', (message) => {
+    if (message.action === 'increment') {
+      process.send({ action: 'increment' });
+    } else if (message.action === 'decrement') {
+      process.send({ action: 'decrement' });
+    }
+  });
+} else {
+  const child1 = fork(__filename, ['child']);
+  const child2 = fork(__filename, ['child']);
 
-def spawn_numbers(pipe):
-    input_pipe, _ = pipe
-    for i in range(10):
-        input_pipe.send(i)
-    input_pipe.close()
+  let sharedArray = [0, 0, 0];
 
+  child1.on('message', (message) => {
+    if (message.action === 'increment') {
+      sharedArray = sharedArray.map((value) => value + 1);
+      console.log(`Shared array incremented by child1: ${sharedArray}`);
+      setTimeout(() => {
+        child1.send({ action: 'increment' });
+      }, 1000);
+    }
+  });
 
-if __name__ == "__main__":
+  child2.on('message', (message) => {
+    if (message.action === 'decrement') {
+      sharedArray = sharedArray.map((value) => value - 1);
+      console.log(`Shared array decremented by child2: ${sharedArray}`);
+      setTimeout(() => {
+        child2.send({ action: 'decrement' });
+      }, 1000);
+    }
+  });
 
-    pipe = multiprocessing.Pipe(True)
-    process = multiprocessing.Process(target=spawn_numbers, args=(pipe,))
-    process.start()
-    process.join()
-
-    _, output_pipe = pipe
-
-    try:
-        while 1:
-            print(output_pipe.recv())
-    except EOFError:
-        print("End")
+  child1.send({ action: 'increment' });
+  child2.send({ action: 'decrement' });
+}
