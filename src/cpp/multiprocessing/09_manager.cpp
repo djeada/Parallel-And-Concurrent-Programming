f9ea5e28641dedf919e824026cb1c13a2eec0b33
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-25 20:29:27 +0200

    Update 09_manager.cpp

--- src/cpp/multiprocessing/09_manager.cpp
+++ src/cpp/multiprocessing/09_manager.cpp
@@ -1,11 +1,58 @@
-import multiprocessing
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <vector>
 
-    def spawn_numbers(pipe) :input_pipe, _ = pipe for i in range(10) :input_pipe.send(i) input_pipe.close()
+const std::string shared_data_file = "shared_data.txt";
 
-                                                                                                              if __name__ == "__main__" :
+class Manager {
+public:
+    Manager(int num_processes) : num_processes_(num_processes), pids_(num_processes) {}
 
-                                                                                                          pipe = multiprocessing.Pipe(True) process = multiprocessing.Process(target = spawn_numbers, args =(pipe, )) process.start() process.join()
+    void start() {
+        for (int i = 0; i < num_processes_; ++i) {
+            pids_[i] = fork();
+            if (pids_[i] == 0) {
+                worker(i);
+                exit(0);
+            }
+        }
 
-                                                                                                                                                                                                                                                       _, output_pipe = pipe
+        for (const auto &pid : pids_) {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
 
-                                                                                                                                                                                                                                                   try : while 1 :print(output_pipe.recv()) except EOFError:print("End")
+    void print_results() {
+        std::cout << "All processes have completed. The contents of the shared file are:" << std::endl;
+        std::ifstream infile(shared_data_file);
+        std::string line;
+        while (std::getline(infile, line)) {
+            std::cout << line << std::endl;
+        }
+        infile.close();
+    }
+
+private:
+    int num_processes_;
+    std::vector<pid_t> pids_;
+
+    void worker(int id) {
+        std::ofstream outfile(shared_data_file, std::ios_base::app);
+        outfile << "Process " << id << " is writing to the shared file." << std::endl;
+        outfile.close();
+    }
+};
+
+int main() {
+    const int num_processes = 3;
+
+    Manager manager(num_processes);
+    manager.start();
+    manager.print_results();
+
+    return 0;
+}
