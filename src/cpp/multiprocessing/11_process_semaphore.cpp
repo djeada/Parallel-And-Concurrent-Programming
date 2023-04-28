1ab9a9805ce412eac572c35609cfb8d5092f7969
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-28 23:38:04 +0200

    Update 11_process_semaphore.cpp

--- src/cpp/multiprocessing/11_process_semaphore.cpp
+++ src/cpp/multiprocessing/11_process_semaphore.cpp
@@ -1,11 +1,58 @@
-import multiprocessing
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <vector>
+#include <random>
+#include <chrono>
+#include <thread>
 
-    def spawn_numbers(pipe) :input_pipe, _ = pipe for i in range(10) :input_pipe.send(i) input_pipe.close()
+// The shared resource is printing to the console
+void shared_resource(int process_id) {
+    std::cout << "Process " << process_id << " is using the shared resource" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(rand() % 500 + 500));
+    std::cout << "Process " << process_id << " is done using the shared resource" << std::endl;
+}
 
-                                                                                                              if __name__ == "__main__" :
+void worker(sem_t *semaphore, int process_id) {
+    std::cout << "Process " << process_id << " is waiting for the semaphore" << std::endl;
+    sem_wait(semaphore);
+    std::cout << "Process " << process_id << " acquired the semaphore" << std::endl;
+    shared_resource(process_id);
+    std::cout << "Process " << process_id << " released the semaphore" << std::endl;
+    sem_post(semaphore);
+}
 
-                                                                                                          pipe = multiprocessing.Pipe(True) process = multiprocessing.Process(target = spawn_numbers, args =(pipe, )) process.start() process.join()
+int main() {
+    const int num_processes = 10;
+    const int max_concurrent = 3;
 
-                                                                                                                                                                                                                                                       _, output_pipe = pipe
+    sem_t *semaphore = sem_open("/semaphore_example", O_CREAT, S_IRUSR | S_IWUSR, max_concurrent);
 
-                                                                                                                                                                                                                                                   try : while 1 :print(output_pipe.recv()) except EOFError:print("End")
+    std::vector<pid_t> children;
+
+    for (int i = 0; i < num_processes; ++i) {
+        pid_t pid = fork();
+
+        if (pid == 0) { // child process
+            worker(semaphore, i);
+            exit(0);
+        } else if (pid > 0) { // parent process
+            children.push_back(pid);
+        } else {
+            std::cerr << "Fork failed" << std::endl;
+            return 1;
+        }
+    }
+
+    // Wait for all child processes to finish
+    for (const auto &child_pid : children) {
+        waitpid(child_pid, nullptr, 0);
+    }
+
+    sem_close(semaphore);
+    sem_unlink("/semaphore_example");
+
+    return 0;
+}
