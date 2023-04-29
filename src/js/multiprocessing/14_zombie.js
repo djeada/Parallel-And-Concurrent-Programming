8a49e822c3d67b81e662c79532caccab9bd0ad5e
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-29 19:27:00 +0200

    Update 14_zombie.js

--- src/js/multiprocessing/14_zombie.js
+++ src/js/multiprocessing/14_zombie.js
@@ -1,24 +1,41 @@
-import multiprocessing
+/*
+This program demonstrates the concept of zombie processes in Node.js. A zombie process is
+a child process that has terminated but has not been reaped by its parent process. The
+operating system keeps an entry in the process table for the terminated child process,
+holding information about its termination status until the parent process collects this
+information.
 
+In this example, we create a child process that terminates but isn't reaped by its
+parent, becoming a zombie. After the child process exits, you can check the process
+list (using commands like 'ps' or 'top') to see that the child process is in a 'zombie'
+state.
 
-def spawn_numbers(pipe):
-    input_pipe, _ = pipe
-    for i in range(10):
-        input_pipe.send(i)
-    input_pipe.close()
+WARNING: This example intentionally creates a zombie process. You should manually kill
+the parent process after running the program to avoid having a zombie process lingering
+on your system.
+*/
 
+const { spawn } = require('child_process');
 
-if __name__ == "__main__":
+if (process.argv[2] === 'child') {
+  // Child process
+  console.log(`Child process (PID: ${process.pid}) started.`);
+  setTimeout(() => {
+    console.log(`Child process (PID: ${process.pid}) finished.`);
+    process.exit(0);
+  }, 2000);
+} else {
+  // Parent process
+  const child = spawn(process.execPath, [__filename, 'child'], {
+    detached: true,
+    stdio: 'inherit'
+  });
 
-    pipe = multiprocessing.Pipe(True)
-    process = multiprocessing.Process(target=spawn_numbers, args=(pipe,))
-    process.start()
-    process.join()
-
-    _, output_pipe = pipe
-
-    try:
-        while 1:
-            print(output_pipe.recv())
-    except EOFError:
-        print("End")
+  console.log(`Parent process (PID: ${process.pid}) started.`);
+  console.log(`Parent process (PID: ${process.pid}) is NOT waiting for the child process to terminate.`);
+  console.log(`Child process (PID: ${child.pid}) will become a zombie after termination.`);
+  
+  setTimeout(() => {
+    console.log(`Parent process (PID: ${process.pid}) finished.`);
+  }, 10000);
+}
