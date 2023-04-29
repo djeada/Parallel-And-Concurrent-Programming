552a50fcd0959aa8712498a694d9932821b5a551
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-29 22:23:02 +0200

    Update 02_future_create_task.cpp

--- src/cpp/asynchrony/02_future_create_task.cpp
+++ src/cpp/asynchrony/02_future_create_task.cpp
@@ -1,29 +1,56 @@
-import asyncio
-import time
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <thread>
 
-"""
-Create a task to start a coroutine in the background.
-Two options:
-- ensure_future() - high level should be used by default
-- create_task() - low level when customizing the loop
-"""
+int slow_square_sync(int x) {
+    std::cout << "Starting slow square computation for " << x << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    int result = x * x;
+    std::cout << "Finished slow square computation for " << x << std::endl;
+    return result;
+}
 
+int slow_square_async(int x) {
+    std::cout << "Starting slow square computation for " << x << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    int result = x * x;
+    std::cout << "Finished slow square computation for " << x << std::endl;
+    return result;
+}
 
-async def example_task():
-    print("example task")
-    await asyncio.sleep(1)
+void synchronous_execution() {
+    auto start_time = std::chrono::steady_clock::now();
 
+    int result1 = slow_square_sync(3);
+    int result2 = slow_square_sync(4);
 
-async def task_generator():
-    for i in range(5):
-        asyncio.ensure_future(example_task())
-    pending = asyncio.all_tasks()
-    for p in pending:
-        print(p)
+    auto elapsed_time = std::chrono::steady_clock::now() - start_time;
+    auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(elapsed_time).count();
 
+    std::cout << "\nSynchronous execution took " << elapsed_seconds << " seconds." << std::endl;
+    std::cout << "Results: " << result1 << ", " << result2 << std::endl;
+}
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(task_generator())
-loop.close()
+void asynchronous_execution() {
+    auto start_time = std::chrono::steady_clock::now();
 
-print("The End")
+    auto future1 = std::async(std::launch::async, slow_square_async, 3);
+    auto future2 = std::async(std::launch::async, slow_square_async, 4);
+
+    int result1 = future1.get();
+    int result2 = future2.get();
+
+    auto elapsed_time = std::chrono::steady_clock::now() - start_time;
+    auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(elapsed_time).count();
+
+    std::cout << "\nAsynchronous execution took " << elapsed_seconds << " seconds." << std::endl;
+    std::cout << "Results: " << result1 << ", " << result2 << std::endl;
+}
+
+int main() {
+    synchronous_execution();
+    asynchronous_execution();
+
+    return 0;
+}
