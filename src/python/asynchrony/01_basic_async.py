d21d01bee0b92c1dcb37e0b7e921183067941a6d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-29 19:41:30 +0200

    Update 01_basic_async.py

--- src/python/asynchrony/01_basic_async.py
+++ src/python/asynchrony/01_basic_async.py
@@ -1,44 +1,53 @@
+"""
+This script demonstrates the difference between synchronous and asynchronous
+approaches to handling I/O-bound tasks using Python. The synchronous version
+runs the tasks sequentially, while the asynchronous version takes advantage of
+asyncio to run the tasks concurrently. This allows for better resource utilization
+and improved performance when dealing with I/O-bound tasks.
+
+The script contains two functions, `sync_main()` and `async_main()`, which
+demonstrate the synchronous and asynchronous approaches, respectively. The
+execution time for each approach is measured and displayed to show the performance
+difference.
+
+To see the difference in execution, simply uncomment the appropriate function call
+in the `if __name__ == "__main__":` block.
+"""
+
+import time
 import asyncio
-import sys
 
-# coroutine that sets the result directly in the future object
-async def sum_n_numbers(future, n):
-    result = 0
-    for i in range(n):
-        result += i + 1
-    future.set_result(f"Sum of n integers result = {result}")
+def sync_task(task_id, sleep_time):
+    print(f"Task {task_id} started.")
+    time.sleep(sleep_time)  # Simulate an I/O-bound task
+    print(f"Task {task_id} finished.")
 
+def sync_main():
+    start_time = time.time()
 
-# coroutine that returns the result like normal function
-async def second_coroutine(n):
-    result = 1
-    for i in range(n - 1):
-        result *= i + 2
+    sync_task(1, 2)
+    sync_task(2, 1)
 
-    return f"{n}! = {result}"
+    elapsed_time = time.time() - start_time
+    print(f"Synchronous execution took {elapsed_time:.2f} seconds.")
 
+async def async_task(task_id, sleep_time):
+    print(f"Task {task_id} started.")
+    await asyncio.sleep(sleep_time)  # Simulate an I/O-bound task
+    print(f"Task {task_id} finished.")
 
-def display_result(future):
-    print(future.result())
+async def async_main():
+    start_time = time.time()
 
+    task1 = async_task(1, 2)
+    task2 = async_task(2, 1)
+
+    # Run the tasks concurrently
+    await asyncio.gather(task1, task2)
+
+    elapsed_time = time.time() - start_time
+    print(f"Asynchronous execution took {elapsed_time:.2f} seconds.")
 
 if __name__ == "__main__":
-
-    n = 5
-
-    loop = asyncio.get_event_loop()
-    future = asyncio.Future()
-
-    # create tasks from coroutines
-    task1 = loop.create_task(sum_n_numbers(future, n))
-    task2 = loop.create_task(second_coroutine(n))
-
-    # callback will be called when the task is completed
-    # task1.add_done_callback(display_result)
-    task2.add_done_callback(display_result)
-
-    loop.run_until_complete(asyncio.wait((task1, task2)))
-
-    display_result(future)
-
-    loop.close()
+    sync_main()
+    asyncio.run(async_main())
