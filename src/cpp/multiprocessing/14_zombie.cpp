f17d2b1f0aef9e0f3c160c569b53d4d9729506e3
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-29 19:26:41 +0200

    Update 14_zombie.cpp

--- src/cpp/multiprocessing/14_zombie.cpp
+++ src/cpp/multiprocessing/14_zombie.cpp
@@ -1,19 +1,43 @@
+/*
+This program demonstrates the concept of zombie processes in C++. A zombie process is
+a child process that has terminated but has not been reaped by its parent process. The
+operating system keeps an entry in the process table for the terminated child process,
+holding information about its termination status until the parent process collects this
+information using a system call (usually wait() or waitpid()).
+
+In this example, we create a child process that terminates but isn't reaped by its
+parent, becoming a zombie. After the child process exits, you can check the process
+list (using commands like 'ps' or 'top') to see that the child process is in a 'zombie'
+state, denoted by the status 'Z+'.
+
+WARNING: This example intentionally creates a zombie process. You should manually kill
+the parent process after running the program to avoid having a zombie process lingering
+on your system.
+*/
+
 #include <iostream>
-#include <thread>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <cstdlib>
 
-auto main() -> int {
-  auto pid = fork();
-  if (pid == 0) {
-    std::cout << "The child process is terminated instantly. " << std::endl;
-    exit(0);
-  } else {
+int main() {
+    pid_t pid = fork();
 
-    int sleepTime = 45;
-    std::cout << "The parent is going to sleep for " << sleepTime << " seconds"
-              << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime * 1000));
-    std::cout << "The parent woke up" << std::endl;
-  }
-  return 0;
+    if (pid == 0) {
+        // Child process
+        std::cout << "Child process (PID: " << getpid() << ") started." << std::endl;
+        sleep(2);
+        std::cout << "Child process (PID: " << getpid() << ") finished." << std::endl;
+        exit(0);
+    } else {
+        // Parent process
+        std::cout << "Parent process (PID: " << getpid() << ") started." << std::endl;
+        std::cout << "Parent process (PID: " << getpid() << ") is NOT waiting for the child process to terminate." << std::endl;
+        std::cout << "Child process (PID: " << pid << ") will become a zombie after termination." << std::endl;
+        sleep(10);
+        std::cout << "Parent process (PID: " << getpid() << ") finished." << std::endl;
+    }
+
+    return 0;
 }
