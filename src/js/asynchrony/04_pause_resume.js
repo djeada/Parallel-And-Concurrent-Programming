7d0a67156a844f2ba4fe6e25c525e1d0af7ecc8e
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-29 22:41:38 +0200

    Update 04_pause_resume.js

--- src/js/asynchrony/04_pause_resume.js
+++ src/js/asynchrony/04_pause_resume.js
@@ -1,41 +1,46 @@
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
-import time
+/*
+This script demonstrates how to pause and resume function execution using
+setInterval, clearInterval, and readline in Node.js. It consists of two main parts:
+1. A long-running function that periodically prints its progress.
+2. A button handler that listens for user input to toggle pause and resume.
 
-executor = ThreadPoolExecutor()
+The script shows how to use Node.js setInterval and clearInterval to
+effectively pause and resume the execution of the long-running function.
+*/
 
-pause_event = asyncio.Event()
+const readline = require('readline');
 
+let paused = false;
+let intervalId;
+let step = 0;
 
-async def long_function():
-    i = 0
-    while True:
-        if pause_event.is_set():
-            while pause_event.is_set():
-                await asyncio.sleep(0.1)
-        print(f"Executing step {i}")
-        i += 1
-        await asyncio.sleep(1)
+const longFunction = () => {
+  console.log(`Executing step ${step}`);
+  step += 1;
+};
 
+const togglePause = () => {
+  paused = !paused;
+  if (paused) {
+    clearInterval(intervalId);
+    console.log('Function paused');
+  } else {
+    intervalId = setInterval(longFunction, 1000);
+    console.log('Function resumed');
+  }
+};
 
-async def button_handler():
-    loop = asyncio.get_event_loop()
-    while True:
-        await loop.run_in_executor(
-            executor, input, "Press ENTER to toggle pause/resume:"
-        )
-        if pause_event.is_set():
-            pause_event.clear()
-        else:
-            pause_event.set()
+const main = () => {
+  intervalId = setInterval(longFunction, 1000);
 
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-async def main():
-    task1 = asyncio.create_task(long_function())
-    task2 = asyncio.create_task(button_handler())
+  rl.on('line', () => {
+    togglePause();
+  });
+};
 
-    await asyncio.gather(task1, task2)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+main();
