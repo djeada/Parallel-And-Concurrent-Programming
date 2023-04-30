0919de9b00a1c19ab8d1b0b006ac5035dbe289f2
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-30 22:38:12 +0200

    Update 06_data_sharing_queue.cpp

--- src/cpp/asynchrony/06_data_sharing_queue.cpp
+++ src/cpp/asynchrony/06_data_sharing_queue.cpp
@@ -1,6 +1,47 @@
-async def example_producer(queue):
-    pass
+/*
+This C++ program demonstrates data sharing and communication between asynchronous tasks
+using the C++ standard library. In this example, we simulate a producer and consumer
+scenario where one task produces data and the other consumes it using std::promise and
+std::future for communication and synchronization.
+*/
 
+#include <iostream>
+#include <thread>
+#include <future>
+#include <chrono>
+#include <random>
 
-async def example_consumer(queue):
-    pass
+int produce_data() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(1, 100);
+    int data = distr(gen);
+    std::cout << "Produced data: " << data << std::endl;
+    return data;
+}
+
+void consume_data(int data) {
+    std::cout << "Consumed data: " << data << std::endl;
+}
+
+int main() {
+    std::promise<int> data_promise;
+    std::future<int> data_future = data_promise.get_future();
+
+    // Producer task
+    auto producer_task = std::async(std::launch::async, [&data_promise]() {
+        int data = produce_data();
+        data_promise.set_value(data);
+    });
+
+    // Consumer task
+    auto consumer_task = std::async(std::launch::async, [&data_future]() {
+        int data = data_future.get();
+        consume_data(data);
+    });
+
+    producer_task.wait();
+    consumer_task.wait();
+
+    return 0;
+}
