abb6708463504edb9d7679613596d81282293cdf
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-30 22:50:52 +0200

    Update 07_semaphore.js

--- src/js/asynchrony/07_semaphore.js
+++ src/js/asynchrony/07_semaphore.js
@@ -1,17 +1,15 @@
-const { Semaphore } = require("async-mutex");
+const { Sema } = require('async-sema');
 
-const resourceSemaphore = new Semaphore(3);
+const resourceSemaphore = new Sema(3);
 
 async function limitedResource(taskId) {
-  const release = await resourceSemaphore.acquire();
-
+  await resourceSemaphore.acquire();
   console.log(`Task ${taskId} is using the limited resource.`);
   await new Promise((resolve) =>
     setTimeout(resolve, Math.floor(Math.random() * 2000) + 500)
   );
   console.log(`Task ${taskId} is done using the limited resource.`);
-
-  release();
+  resourceSemaphore.release();
 }
 
 async function main() {
