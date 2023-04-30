66c33cb250a3c9130816c6edc175d9ee527e5701
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-30 22:47:07 +0200

    Update 07_semaphore.js

--- src/js/asynchrony/07_semaphore.js
+++ src/js/asynchrony/07_semaphore.js
@@ -1,33 +1,27 @@
 const { Semaphore } = require("async-mutex");
 
-async function resourceLimitedOperation(id) {
-  console.log(`Worker ${id} is entering the resource-limited operation`);
+const resourceSemaphore = new Semaphore(3);
+
+async function limitedResource(taskId) {
+  const release = await resourceSemaphore.acquire();
+
+  console.log(`Task ${taskId} is using the limited resource.`);
   await new Promise((resolve) =>
-    setTimeout(resolve, Math.random() * 1500 + 500)
+    setTimeout(resolve, Math.floor(Math.random() * 2000) + 500)
   );
-  console.log(`Worker ${id} is leaving the resource-limited operation`);
+  console.log(`Task ${taskId} is done using the limited resource.`);
+
+  release();
 }
 
-async function worker(sem, id) {
-  for (let i = 0; i < 3; i++) {
-    await sem.runExclusive(async () => {
-      await resourceLimitedOperation(id);
-    });
-    await new Promise((resolve) =>
-      setTimeout(resolve, Math.random() * 500 + 500)
-    );
+async function main() {
+  const tasks = [];
+
+  for (let i = 0; i < 10; i++) {
+    tasks.push(limitedResource(i));
   }
-  console.log(`End of worker ${id} function`);
+
+  await Promise.all(tasks);
 }
 
-async function taskGenerator() {
-  const sem = new Semaphore(2); // Limit concurrency to 2
-  const nWorkers = 5;
-  const workers = Array.from({ length: nWorkers }, (_, i) => worker(sem, i));
-  await Promise.all(workers);
-}
-
-(async () => {
-  await taskGenerator();
-  console.log("The End");
-})();
+main();
