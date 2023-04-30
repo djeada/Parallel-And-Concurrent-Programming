cede0ab79857452f531381d0583c9629a9e444a6
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-30 22:47:31 +0200

    Update 08_producer_consumer.js

--- src/js/asynchrony/08_producer_consumer.js
+++ src/js/asynchrony/08_producer_consumer.js
@@ -1,30 +1,65 @@
-import asyncio
-import random
+/*
+This script demonstrates the producer-consumer pattern using Node.js's async and promises.
+The producer creates items and puts them into a queue, while the consumer takes items from the queue and processes them.
+This pattern is useful when you want to separate the creation and processing of items, allowing them to run concurrently.
+*/
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-async def producer(queue):
-    while True:
-        item = random.randint(1, 10)
-        print(f"Producing {item}")
-        await queue.put(item)
-        await asyncio.sleep(random.random())
+class AsyncQueue {
+  constructor() {
+    this.queue = [];
+    this.resolveQueue = [];
+  }
 
+  put(item) {
+    if (this.resolveQueue.length > 0) {
+      this.resolveQueue.shift()(item);
+    } else {
+      this.queue.push(item);
+    }
+  }
 
-async def consumer(queue):
-    while True:
-        item = await queue.get()
-        print(f"Consuming {item}")
-        await asyncio.sleep(random.random())
+  async get() {
+    if (this.queue.length > 0) {
+      return this.queue.shift();
+    } else {
+      return new Promise((resolve) => this.resolveQueue.push(resolve));
+    }
+  }
+}
 
+async function producer(queue, producerId) {
+  for (let i = 0; i < 5; ++i) {
+    const item = `Item ${i} from producer ${producerId}`;
+    queue.put(item);
+    console.log(`Producer ${producerId} produced ${item}`);
+    await sleep(Math.random() * 1500 + 500);
+  }
+}
 
-async def main():
-    queue = asyncio.Queue(maxsize=5)
+async function consumer(queue, consumerId) {
+  for (let i = 0; i < 5; ++i) {
+    const item = await queue.get();
+    console.log(`Consumer ${consumerId} consumed ${item}`);
+    await sleep(Math.random() * 1500 + 500);
+  }
+}
 
-    producers = [asyncio.create_task(producer(queue)) for _ in range(2)]
-    consumers = [asyncio.create_task(consumer(queue)) for _ in range(4)]
+async function main() {
+  const queue = new AsyncQueue();
 
-    await asyncio.gather(*producers, *consumers)
+  const producers = [];
+  for (let i = 0; i < 3; ++i) {
+    producers.push(producer(queue, i));
+  }
 
+  const consumers = [];
+  for (let i = 0; i < 3; ++i) {
+    consumers.push(consumer(queue, i));
+  }
 
-if __name__ == "__main__":
-    asyncio.run(main())
+  await Promise.all([...producers, ...consumers]);
+}
+
+main();
