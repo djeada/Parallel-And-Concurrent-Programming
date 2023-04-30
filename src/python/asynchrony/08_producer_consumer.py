8feefdfffba38b72389cdd04e159648fcd4e48da
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-30 22:42:49 +0200

    Update 08_producer_consumer.py

--- src/python/asynchrony/08_producer_consumer.py
+++ src/python/asynchrony/08_producer_consumer.py
@@ -1,29 +1,43 @@
+"""
+This script demonstrates the producer-consumer pattern using Python's asyncio library.
+The producer creates items and puts them into a queue, while the consumer takes items from the queue and processes them.
+This pattern is useful when you want to separate the creation and processing of items, allowing them to run concurrently.
+"""
+
 import asyncio
 import random
 
-
-async def producer(queue):
-    while True:
-        item = random.randint(1, 10)
-        print(f"Producing {item}")
+async def producer(queue, producer_id):
+    for i in range(5):
+        item = f"Item {i} from producer {producer_id}"
         await queue.put(item)
-        await asyncio.sleep(random.random())
+        print(f"Producer {producer_id} produced {item}")
+        await asyncio.sleep(random.uniform(0.5, 2))
 
 
-async def consumer(queue):
+async def consumer(queue, consumer_id):
     while True:
         item = await queue.get()
-        print(f"Consuming {item}")
-        await asyncio.sleep(random.random())
+        if item is None:
+            break
+        print(f"Consumer {consumer_id} consumed {item}")
+        await asyncio.sleep(random.uniform(0.5, 2))
+        queue.task_done()
 
 
 async def main():
-    queue = asyncio.Queue(maxsize=5)
+    queue = asyncio.Queue()
 
-    producers = [asyncio.create_task(producer(queue)) for _ in range(2)]
-    consumers = [asyncio.create_task(consumer(queue)) for _ in range(4)]
+    producers = [asyncio.create_task(producer(queue, i)) for i in range(3)]
+    consumers = [asyncio.create_task(consumer(queue, i)) for i in range(2)]
 
-    await asyncio.gather(*producers, *consumers)
+    await asyncio.gather(*producers)
+    await queue.join()
+
+    for _ in consumers:
+        await queue.put(None)
+
+    await asyncio.gather(*consumers)
 
 
 if __name__ == "__main__":
