27de3bbe5692b1fd53a8acfa4746a3fa20d08d0e
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-30 23:03:11 +0200

    Update 09_fetch_parallel.js

--- src/js/asynchrony/09_fetch_parallel.js
+++ src/js/asynchrony/09_fetch_parallel.js
@@ -1,29 +1,65 @@
-import aiohttp
-import asyncio
+const http = require("http");
+const https = require("https");
 
+const URLS = [
+  "http://httpbin.org/get",
+  "https://api.github.com",
+  "https://www.example.com",
+];
 
-async def fetch(url, session):
-    async with session.get(url) as response:
-        content = await response.text()
-        print(f"Fetched content from {url} with length {len(content)}")
+function fetchUrl(url) {
+  return new Promise((resolve, reject) => {
+    const protocol = url.startsWith("https") ? https : http;
+    protocol
+      .get(url, (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          resolve(data);
+        });
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+}
 
+async function fetchSync(url) {
+  const response = await fetchUrl(url);
+  console.log(`Synchronously fetched content from ${url}`);
+  return response;
+}
 
-async def fetch_all(urls):
-    async with aiohttp.ClientSession() as session:
-        tasks = [fetch(url, session) for url in urls]
-        await asyncio.gather(*tasks)
+async function fetchAllSync(urls) {
+  const contents = [];
+  for (const url of urls) {
+    contents.push(await fetchSync(url));
+  }
+  return contents;
+}
 
+async function fetchAsync(url) {
+  return fetchSync(url);
+}
 
-async def main():
-    urls = [
-        "https://httpbin.org/get",
-        "https://api.github.com",
-        "https://www.python.org",
-        "https://www.example.com",
-    ]
+async function fetchAllAsync(urls) {
+  const promises = urls.map((url) => fetchAsync(url));
+  const contents = await Promise.all(promises);
+  return contents;
+}
 
-    await fetch_all(urls)
+async function main() {
+  const startTimeSync = Date.now();
+  const contentsSync = await fetchAllSync(URLS);
+  const elapsedTimeSync = (Date.now() - startTimeSync) / 1000;
+  console.log(`\nSynchronous execution took ${elapsedTimeSync} seconds.`);
 
+  const startTimeAsync = Date.now();
+  const contentsAsync = await fetchAllAsync(URLS);
+  const elapsedTimeAsync = (Date.now() - startTimeAsync) / 1000;
+  console.log(`\nAsynchronous execution took ${elapsedTimeAsync} seconds.`);
+}
 
-if __name__ == "__main__":
-    asyncio.run(main())
+main();
