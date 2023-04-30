935a4f30526988b76e103037575868b7ed5f658d
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-30 23:07:01 +0200

    Update 10_mutex.js

--- src/js/asynchrony/10_mutex.js
+++ src/js/asynchrony/10_mutex.js
@@ -1,32 +1,44 @@
-import asyncio
-import random
+/*
+This script demonstrates the use of async-lock (a.k.a. async mutex) to protect
+access to shared resources in an asynchronous environment. In this example, we
+simulate a bank account with two async functions attempting to transfer money concurrently.
+The use of an async mutex (lock) is crucial to prevent race conditions that could lead
+to incorrect account balances.
+*/
 
+const AsyncLock = require('async-lock');
+const lock = new AsyncLock();
 
-async def critical_section(id):
-    print(f"Worker {id} is entering the critical section")
-    await asyncio.sleep(random.uniform(0.5, 2))
-    print(f"Worker {id} is leaving the critical section")
+class BankAccount {
+  constructor(balance) {
+    this.balance = balance;
+  }
 
+  async transfer(amount) {
+    await lock.acquire('balance', async () => {
+      console.log(`Transferring ${amount}...`);
+      await new Promise((resolve) => setTimeout(resolve, 100)); // Simulate some processing time
+      this.balance += amount;
+      console.log(`Transfer of ${amount} complete. New balance: ${this.balance}`);
+    });
+  }
+}
 
-async def foo(lock, id):
-    for _ in range(3):
-        async with lock:
-            await critical_section(id)
-        await asyncio.sleep(random.uniform(0.5, 1))
-    print(f"End of worker {id} function")
+async function transferMoney(account, amounts) {
+  for (const amount of amounts) {
+    await account.transfer(amount);
+  }
+}
 
+async function main() {
+  const account = new BankAccount(100);
 
-async def task_generator():
-    lock = asyncio.Lock()
-    n_workers = 3
-    await asyncio.gather(*[foo(lock, i) for i in range(n_workers)])
+  const transfer1 = transferMoney(account, [50, -20, 30]);
+  const transfer2 = transferMoney(account, [-10, 60, -10]);
 
+  await Promise.all([transfer1, transfer2]);
 
-async def main():
-    await task_generator()
+  console.log(`Final account balance: ${account.balance}`);
+}
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
-
-print("The End")
+main();
