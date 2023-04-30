3f85f10a4200d31d24cd31599918232769bcef0a
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-04-30 22:38:36 +0200

    Update 06_data_sharing_queue.js

--- src/js/asynchrony/06_data_sharing_queue.js
+++ src/js/asynchrony/06_data_sharing_queue.js
@@ -1,6 +1,28 @@
-async def example_producer(queue):
-    pass
+/*
+This Node.js script demonstrates data sharing and communication between asynchronous tasks
+using JavaScript Promises. In this example, we simulate a producer and consumer
+scenario where one task produces data and the other consumes it using Promises for
+communication and synchronization.
+*/
 
+const produceData = () => {
+  const data = Math.floor(Math.random() * 100) + 1;
+  console.log(`Produced data: ${data}`);
+  return data;
+};
 
-async def example_consumer(queue):
-    pass
+const consumeData = (data) => {
+  console.log(`Consumed data: ${data}`);
+};
+
+const main = async () => {
+  const dataPromise = new Promise((resolve) => {
+    const data = produceData();
+    resolve(data);
+  });
+
+  const data = await dataPromise;
+  consumeData(data);
+};
+
+main();
