d32eff30e535262c6bbae7599e3866ed610dfc36
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-05-01 18:52:33 +0200

    Update 11_barrier.py

--- src/python/asynchrony/11_barrier.py
+++ src/python/asynchrony/11_barrier.py
@@ -1,28 +1,32 @@
+"""
+This script demonstrates the use of asyncio.Barrier in Python. asyncio.Barrier is
+a synchronization primitive that can be used to coordinate multiple async tasks.
+In this example, we have a set of tasks that each perform some work and then
+wait for all tasks to reach the barrier before continuing. This ensures that
+the tasks move to the next phase of their work in a coordinated fashion.
+"""
+
 import asyncio
+import random
 
+async def task_worker(task_id, barrier):
+    print(f"Task {task_id} starting phase 1...")
+    await asyncio.sleep(random.randint(1, 5))  # Simulate some processing time
+    print(f"Task {task_id} finished phase 1")
+    
+    await barrier.wait()  # Wait for all tasks to reach the barrier
 
-async def foo():
-    for i in range(5):
-        print("foo")
-        await asyncio.sleep(1)
+    print(f"Task {task_id} starting phase 2...")
+    await asyncio.sleep(random.randint(1, 5))  # Simulate some processing time
+    print(f"Task {task_id} finished phase 2")
 
+async def main():
+    num_tasks = 5
+    barrier = asyncio.Barrier(num_tasks)
 
-async def bar():
-    for i in range(10):
-        print("bar")
-        await asyncio.sleep(1)
+    tasks = [asyncio.create_task(task_worker(i, barrier)) for i in range(num_tasks)]
 
+    await asyncio.gather(*tasks)
 
-loop = asyncio.get_event_loop()
-
-try:
-    asyncio.ensure_future(foo())
-    asyncio.ensure_future(bar())
-    loop.run_forever()
-except KeyboardInterrupt:
-    # use Ctrl+C to close the program
-    pass
-finally:
-    loop.close()
-
-print("The End")
+if __name__ == "__main__":
+    asyncio.run(main())
