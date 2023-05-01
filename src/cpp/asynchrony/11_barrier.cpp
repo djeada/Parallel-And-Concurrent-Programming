bc246d5103a45e75d8a537f35641ff81f3a79e34
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-05-01 18:56:30 +0200

    Update 11_barrier.cpp

--- src/cpp/asynchrony/11_barrier.cpp
+++ src/cpp/asynchrony/11_barrier.cpp
@@ -1,28 +1,55 @@
-import asyncio
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <thread>
+#include <vector>
+#include <barrier>
+#include <coroutine>
 
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> dis(1, 5);
 
-async def foo():
-    for i in range(5):
-        print("foo")
-        await asyncio.sleep(1)
+class task_worker {
+public:
+    task_worker(int task_id, std::barrier<>* barrier)
+        : task_id_(task_id), barrier_(barrier) {}
 
+    void operator()() {
+        phase_1();
+        barrier_->arrive_and_wait();
+        phase_2();
+    }
 
-async def bar():
-    for i in range(10):
-        print("bar")
-        await asyncio.sleep(1)
+private:
+    void phase_1() {
+        std::cout << "Task " << task_id_ << " starting phase 1..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(dis(gen)));
+        std::cout << "Task " << task_id_ << " finished phase 1" << std::endl;
+    }
 
+    void phase_2() {
+        std::cout << "Task " << task_id_ << " starting phase 2..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(dis(gen)));
+        std::cout << "Task " << task_id_ << " finished phase 2" << std::endl;
+    }
 
-loop = asyncio.get_event_loop()
+    int task_id_;
+    std::barrier<>* barrier_;
+};
 
-try:
-    asyncio.ensure_future(foo())
-    asyncio.ensure_future(bar())
-    loop.run_forever()
-except KeyboardInterrupt:
-    # use Ctrl+C to close the program
-    pass
-finally:
-    loop.close()
+int main() {
+    const int num_tasks = 5;
+    std::barrier<> barrier(num_tasks);
+    std::vector<std::thread> tasks;
 
-print("The End")
+    for (int i = 0; i < num_tasks; ++i) {
+        tasks.emplace_back(task_worker(i, &barrier));
+    }
+
+    for (auto& task : tasks) {
+        task.join();
+    }
+
+    return 0;
+}
