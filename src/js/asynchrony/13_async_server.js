6f2d86e6e29a0d21c1f80ad00c1b7ddd47304193
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-05-01 19:59:42 +0200

    Update 13_async_server.js

--- src/js/asynchrony/13_async_server.js
+++ src/js/asynchrony/13_async_server.js
@@ -1,22 +1,26 @@
-import asyncio
+/*
+This example demonstrates how to create a simple HTTP server using Node.js
+and its built-in 'http' module. The server listens for incoming requests and
+asynchronously sends a response. This example shows the basics of creating
+an async server and handling incoming requests.
+*/
 
+const http = require("http");
 
-async def foo():
-    for i in range(5):
-        print("foo")
-        await asyncio.sleep(1)
+const server = http.createServer((req, res) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
 
+  // Set response headers
+  res.setHeader("Content-Type", "text/plain");
 
-async def bar():
-    for i in range(10):
-        print("bar")
-        await asyncio.sleep(1)
+  // Asynchronously wait before sending a response
+  setTimeout(() => {
+    res.statusCode = 200;
+    res.end("Hello, World!\n");
+  }, 1000);
+});
 
-
-loop = asyncio.get_event_loop()
-
-# The event loop is executed until the task is completed.
-loop.run_until_complete(foo())
-loop.run_until_complete(bar())
-
-loop.close()
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
