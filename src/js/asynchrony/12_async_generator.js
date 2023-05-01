5f3b49ed2ee3036c5c6d52df9c3ae857205e586c
Adam Djellouli <37275728+djeada@users.noreply.github.com>
2023-05-01 19:05:10 +0200

    Update 12_async_generator.js

--- src/js/asynchrony/12_async_generator.js
+++ src/js/asynchrony/12_async_generator.js
@@ -1,22 +1,14 @@
-import asyncio
+async function* asyncGenerator() {
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+    for (let i = 0; i < 5; i++) {
+        await delay(Math.random() * 1000 + 500);
+        yield i;
+    }
+}
 
-async def foo():
-    for i in range(5):
-        print("foo")
-        await asyncio.sleep(1)
-
-
-async def bar():
-    for i in range(10):
-        print("bar")
-        await asyncio.sleep(1)
-
-
-loop = asyncio.get_event_loop()
-
-# The event loop is executed until the task is completed.
-loop.run_until_complete(foo())
-loop.run_until_complete(bar())
-
-loop.close()
+(async () => {
+    for await (const value of asyncGenerator()) {
+        console.log("Received value:", value);
+    }
+})();
